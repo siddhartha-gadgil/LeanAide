7f07e4f3ad960e11d3806d24eca8812f14e89b80
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-06-16 21:29:45 +0530

    lakefile etc for running

--- Main.lean
+++ Main.lean
@@ -1,4 +1,10 @@
 import LeanCodePrompts
+import LeanCodePrompts.CheckParse
+
+set_option maxHeartbeats 10000000
+set_option maxRecDepth 1000
+set_option compiler.extract_closed false
+
 
 def main : IO Unit :=
   IO.println s!"Hello, {hello}!"
--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,9 @@
 import Lake
 open Lake DSL
 
-package LeanCodePrompts
+package LeanCodePrompts{
+  supportInterpreter := true
+}
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"master"
