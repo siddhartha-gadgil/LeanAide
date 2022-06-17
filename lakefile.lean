15441690b9afc60bcf0428688396fc91c072b025
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-06-17 06:33:19 +0530

    Revert "Add Mathlib3 dependency"
    This reverts commit 18ea020bd870412198c85923d13af32c09b4bf74.

--- lakefile.lean
+++ lakefile.lean
@@ -7,6 +7,3 @@ package LeanCodePrompts{
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"master"
-
-require mathlib3port from git
-  "https://github.com/leanprover-community/mathlib3port.git"@"master"
