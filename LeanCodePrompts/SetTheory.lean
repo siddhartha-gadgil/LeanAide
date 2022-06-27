56de8541f8fe9f72c2a424130644818e10763c4c
ART0 <18333981+0Art0@users.noreply.github.com>
2022-06-27 21:37:28 +0530

    Additional theorems about sets

--- LeanCodePrompts/SetTheory.lean
+++ LeanCodePrompts/SetTheory.lean
@@ -186,8 +186,114 @@ The empty set is a subset of every set.
 theorem empty_subset : ∅ ⊆ X := by intro _ h; contradiction 
 
 /-
+The empty set contains no elements.
+-/
+theorem empty_set_no_elem : ∀ {a : α}, ¬(a ∈ (∅ : Set α)) := by 
+  intro _ h
+  have : false = true := h 
+  contradiction
+
+/-
+The union of two sets contains the individual sets.
+-/
+theorem union_subset : X ⊆ (X ∪ Y) ∧ Y ⊆ (X ∪ Y) := ⟨.inl, .inr⟩
+
+/-
+The intersection of two sets is contained in the individual sets.
+-/
+theorem intersection_subset : (X ∩ Y) ⊆ X ∧ (X ∩ Y) ⊆ Y := ⟨And.left, And.right⟩
+
+/-
+The intersection of two sets is contained in the union of the two sets.
+-/
+
+/-
 The pre-image of a set under a function.
 -/
 def preimage (f : α → β) (X : Set β) : Set α := {a | f a ∈ X}
+
+postfix:max "⁻¹" => preimage
+
+/-
+The pre-image of a set under the identity function is the set itself.
+-/
+theorem preimage_id (X : Set α) : id⁻¹ X = X := rfl 
+
+/-
+The image of a set under a function is the set itself.
+-/
+theorem image_id (X : Set α) : id <$> X = X := by simp 
+
+/-
+The pre-image of a set under the composition of two functions is the pre-image of the pre-image.
+-/
+theorem preimage_comp (f : α → β) (g : β → γ) (X : Set γ) : ((g ∘ f)⁻¹ X) = (f⁻¹ (g⁻¹ X)) := rfl
+
+/-
+The image of a set under the composition of two functions is the image of the image.
+-/
+theorem image_comp (f : α → β) (g : β → γ) (X : Set α) : (g <$> (f <$> X)) = ((g ∘ f) <$> X) := by
+  simp only [Functor.map, image]
+  ext 
+  simp [setOf]
+  intro
+  apply propext
+  apply Iff.intro
+  · intro ⟨b, ⟨⟨a, ⟨h₁, h₂⟩⟩, h₃⟩⟩
+    exact ⟨a, ⟨h₁, h₂ ▸ h₃⟩⟩
+  · intro ⟨a, ⟨h₁, h₂⟩⟩
+    exact ⟨f a, ⟨⟨a, ⟨h₁, rfl⟩⟩, h₂⟩⟩
+
+/-
+The pre-image of a union of sets is the union of the pre-images.
+-/
+theorem preimage_union (f : α → β) (S : Set (Set β)) : f⁻¹ (⋃₀ S) = ⋃₀ {f⁻¹ X | X ∈ S} := by
+  simp [preimage, Functor.map]
+  ext
+  intro
+  simp [setOf, sUnion]
+  apply propext
+  apply Iff.intro
+  · intro ⟨a, ⟨h₁, h₂⟩⟩
+    apply Exists.intro (f⁻¹ a)
+    apply And.intro
+    · exact ⟨a, ⟨h₁, rfl⟩⟩
+    · apply h₂
+  · intro ⟨a, ⟨⟨X, ⟨h₁, h₂⟩⟩, h₃⟩⟩
+    apply Exists.intro X
+    apply And.intro
+    · exact h₁
+    · revert h₃
+      rw [← h₂]
+      exact id 
+
+/-
+The image of a union of sets is the union of the images.
+-/
+theorem image_union (f : α → β) (S : Set (Set α)) : (f <$> (⋃₀ S)) = (⋃₀ ((Functor.map f) <$> S)) := by
+  simp [image, Functor.map]
+  ext
+  simp [setOf, sUnion]
+  intro
+  apply propext
+  apply Iff.intro
+  · intro ⟨a, ⟨⟨X, ⟨h₁, h₂⟩⟩, h₃⟩⟩
+    apply Exists.intro (f <$> X)
+    apply And.intro
+    · apply Exists.intro X 
+      apply And.intro
+      · exact h₁
+      · simp [Functor.map, image]
+        ext 
+        intro
+        simp [setOf]
+    · exact ⟨a, ⟨h₂, h₃⟩⟩ 
+  · intro ⟨b, ⟨⟨X, ⟨h₁, h₂⟩⟩, h₃⟩⟩
+    rw [← h₂] at h₃
+    have ⟨c, ⟨h₄, h₅⟩⟩ := h₃
+    apply Exists.intro c
+    apply And.intro
+    · exact ⟨X, ⟨h₁, h₄⟩⟩
+    · exact h₅
 
 end Set
