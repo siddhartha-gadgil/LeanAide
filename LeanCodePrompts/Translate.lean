1e4f9108643f475eb5d197f2e94e8ab20c57a27f
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-08-04 12:18:42 +0530

    updated for text as a field

--- LeanCodePrompts/Translate.lean
+++ LeanCodePrompts/Translate.lean
@@ -22,6 +22,12 @@ def egBlob := "[\"{p : ℕ} (hp : Nat.Prime p) :  p = 2 ∨ p % 2 = 1 \",
    \"{p : ℕ} (hp : Nat.Prime p) : p = 2 ∨ p % 2 = 1 \",
    \"Nonsense output to test filtering\"]"
 
+def egBlob' := "[{ \"text\" : \"{p : ℕ} (hp : Nat.Prime p) :  p = 2 ∨ p % 2 = 1 \"},
+   { \"text\" : \"(p : ℕ) :  Nat.Prime p ↔ p = 2 ∨ p % 2 = 1 \"},
+   { \"text\" : \"{p : ℕ} (hp : Nat.Prime p) : p = 2 ∨ p % 2 = 1 \"},
+   { \"text\" : \"(n : ℕ) (hp : Nat.Prime n) : n = 2 ∨ n % 2 = 1 \"},
+   { \"text\" : \"{p : ℕ} (hp : Nat.Prime p) : p = 2 ∨ p % 2 = 1 \"},
+   { \"text\" : \"Nonsense output to test filtering\"}]"
 
 def arrayToExpr (output: Array String) : TermElabM Expr := do
   let elaborated ← output.filterM  <| 
@@ -56,11 +62,30 @@ def textToExpr (s: String) : TermElabM Expr := do
   let output := outArr
   arrayToExpr output
 
+def textToExpr' (s: String) : TermElabM Expr := do
+  let json ← readJson s
+  let outArr : Array String ← 
+    match json.getArr? with
+    | Except.ok arr => 
+        let parsedArr : Array String ← 
+          arr.filterMapM <| fun js =>
+            match js.getObjVal? "text" with
+              | Except.ok jsstr =>
+                match jsstr.getStr? with
+                | Except.ok str => pure (some str)
+                | Except.error e => 
+                  throwError m!"json string expected but got {js}, error: {e}"
+              | Except.error _ =>
+                throwError m!"no text field"
+        pure parsedArr
+    | Except.error e => throwError m!"json parsing error: {e}"
+  let output := outArr
+  arrayToExpr output
 
 elab "//-" cb:commentBody : term => do
   let s := cb.getAtomVal!
   let s := s.dropRight 2
-  let e ← textToExpr egBlob
+  let e ← textToExpr' egBlob'
   logInfo m!"{e}"
   return e
 
