f4e16585bbfbb9ed876dc28b63724df88b6af3dd
ART0 <18333981+0Art0@users.noreply.github.com>
2022-08-22 23:37:41 +0530

    Sentence sim pairs

--- LeanCodePrompts/KeywordSummary/KeywordExtraction.lean
+++ LeanCodePrompts/KeywordSummary/KeywordExtraction.lean
@@ -2,10 +2,11 @@ import Lean
 
 namespace Lean.Json
 
-def parseFile (path : System.FilePath) : IO <| Array Json := do
-  IO.ofExcept $ Json.parse (← IO.FS.readFile path) >>= Json.getArr?
+def parseArrIO (s : String) : IO <| Array Json := do
+  IO.ofExcept $ Json.parse s >>= Json.getArr?
 
-#eval Json.parse "{\"key\": \"val\"}"
+def parseFile (path : System.FilePath) : IO <| Array Json :=
+  IO.FS.readFile path >>= Json.parseArrIO
 
 instance : GetElem Json String Json (λ j k => Except.toBool <| j.getObjVal? k) where
   getElem := λ j key h =>
@@ -22,7 +23,7 @@ open Lean IO
 section MathlibStatements
 
 def MathlibStatements : IO <| Array Json := 
-  Json.parseFile "OpenaiModelPrompting/KeywordSummary/full_mathlib_keyword_summary.json"
+  Json.parseFile "LeanCodePrompts/KeywordSummary/full_mathlib_keyword_summary.json"
 
 def fetchRelevantStatements (kw : String) : IO <| Array Json := do
   return (← MathlibStatements).filter $ (Option.toBool <| ·["keywords"]? >>= (·[kw]?))
@@ -123,3 +124,13 @@ def statement := "Every finite integral domain is a field."
 #eval yakeOutput statement
 
 end Experiments
+
+
+def egJsonSentenceSim : String := "[{\"score\": 0.7298493385314941, \"dct\": {\"name\": \"nat.prime.mod_two_eq_one_iff_ne_two\", \"statement\": \"theorem nat.prime.mod_two_eq_one_iff_ne_two {p : ℕ} [fact (nat.prime p)] : p % 2 = 1 ↔ p ≠ 2\", \"theorem\": \"{p : ℕ} [fact (nat.prime p)] : p % 2 = 1 ↔ p ≠ 2\", \"args\": \"{p : ℕ} [fact (nat.prime p)]\", \"doc_string\": \"A prime `p` satisfies `p % 2 = 1` if and only if `p ≠ 2`.\", \"type\": \"p % 2 = 1 ↔ p ≠ 2\"}}, {\"score\": 0.57069331407547, \"dct\": {\"name\": \"nat.odd_mod_four_iff\", \"statement\": \"theorem nat.odd_mod_four_iff {n : ℕ} : n % 2 = 1 ↔ n % 4 = 1 ∨ n % 4 = 3\", \"theorem\": \"{n : ℕ} : n % 2 = 1 ↔ n % 4 = 1 ∨ n % 4 = 3\", \"args\": \"{n : ℕ}\", \"doc_string\": \"A natural number is odd iff it has residue `1` or `3` mod `4`\", \"type\": \"n % 2 = 1 ↔ n % 4 = 1 ∨ n % 4 = 3\"}}, {\"score\": 0.5074306726455688, \"dct\": {\"name\": \"nat.factorization_eq_zero_iff\", \"statement\": \"theorem nat.factorization_eq_zero_iff (n : ℕ) : n.factorization = 0 ↔ n = 0 ∨ n = 1\", \"theorem\": \"(n : ℕ) : n.factorization = 0 ↔ n = 0 ∨ n = 1\", \"args\": \"(n : ℕ)\", \"doc_string\": \"The only numbers with empty prime factorization are `0` and `1`\", \"type\": \"n.factorization = 0 ↔ n = 0 ∨ n = 1\"}}, {\"score\": 0.4935227930545807, \"dct\": {\"name\": \"nat.prime.factorization\", \"statement\": \"theorem nat.prime.factorization {p : ℕ} (hp : nat.prime p) : p.factorization = finsupp.single p 1\", \"theorem\": \"{p : ℕ} (hp : nat.prime p) : p.factorization = finsupp.single p 1\", \"args\": \"{p : ℕ} (hp : nat.prime p)\", \"doc_string\": \"The only prime factor of prime `p` is `p` itself, with multiplicity `1`\", \"type\": \"p.factorization = finsupp.single p 1\"}}]"
+
+def sentenceSimPairs (s : String) : IO <| Array (String × String) := do
+  (← Json.parseArrIO s).mapM (
+    let dict := ·["dct"]!
+    return (← IO.ofExcept <| dict["doc_string"]!.getStr?, ← IO.ofExcept <| dict["theorem"]!.getStr?) )
+
+#eval sentenceSimPairs egJsonSentenceSim
