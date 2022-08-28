e9523a3011955819ef0cc4746e95b5c55055c71e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-08-28 13:07:25 +0530

    shorter demo comments

--- LeanCodePrompts/TranslateExample.lean
+++ LeanCodePrompts/TranslateExample.lean
@@ -9,78 +9,42 @@ example : //- Every prime number is either `2` or even -/  := by
 
 #eval showLogs 2
 
-#check repr
-
-/-!
-## Codex responses
-
-* We obtain 5 best responses from Codex.
-* We post-process and choose.
-
-## Interface
-
-* Based on an elaborator.
-* In the longer run, we would like to edit the source code (using widgets/code-actions).
-* Caching of results and polling for pending queries is done to reduce server calls.
-
-## Input dependent prompting
-
-* The prompt has examples of docstrings and translation to lean.
-* The examples are from a database extracted from `mathlib` using `docgen`.
-* We choose prompts based on __sentence similarity__.
-
-## Post-processing and Lean 4
-
-* The examples and codex output are in Lean 3.
-* Some macros are used to bridge syntactic differences.
-* Identifiers are mapped using a database of correspondence between `mathlib` and `binport`:
-    - names were extracted from mathlib and binport (the latter as constants in the environment).
-    - the nearest match was taken, with __case transformations__ and prefixing or not with __is__ and __has__ and adding `ₓ` or `ₓₓ`.
-
-## Filtering and Selection
-
-* We filter only valid responses.
-* If there are no valid answers, we return a sorry `Prop` and also give translations in the infoview.
-* We attempt to group by equality (a little more than definitional equality) and choose the largest group.
--/
-
-
 example : //- Every field is a ring -/ := by 
        sorry
 
 #eval showLogs 2
 
-/-!
-## Auto-correction
+example : //- There are infinitely many odd numbers -/ := by
+        sorry
 
-* We attempt to map unknown identifiers to known ones.
-* For now, this is by case transformations, and adding/removing "is" and "has"
-* These are the most common errors we see. The other common error is using shorter or longer forms (e.g. `adj` vs `adjacency`).
-* This and the Lean 3  -> Lean 4 name mapping (here) are done in Lean.
--/
-
--- example : //- There are infinitely many odd natural numbers -/ := by sorry
-
--- example : //- Every subgroup of a free group is a free group -/ := by
---             sorry
-
--- universe u v
-
--- #check {  α :  Type  u } ->  {  G :  Type  v } ->  [  Groupₓ  G ] ->  [  IsFreeGroup   G] ->  (  H :  Subgroup  G ) ->  FreeGroup  G
+#eval showLogs 2
 
 /-!
-## Workpoints
+## Features
 
-* Hyperparameter tuning.
-* Round trip to select and validate translations.
-* Two stage translation via an intermediate prompt.
-* Diverse prompts via repulsion.
-* Hybrid prompts: fixed, input domain based and sentence similarity based. 
-* Prompts using other definitions in the environments, especially ones nearby.
-* Better recognition of equality/equivalence, for example using Aesop.
-* Allow users to give sample prompts.
-* Allow for crowdsourcing sample prompts.
-* Expand __auto-correction__, in particular allowing reordering of arguments.
-* Suggest imports.
-* Port more code to Lean.
+* Integrated interface using elaboration; caching, polling.
+* Input-dependent prompting:
+    - database of doc-strings from Mathlib
+    - selected using: sentence similarity, keywords
+* Post-processing:
+    - Lean 3 to Lean 4 translation, auto-correction.
+        - based on case-transformations, adding/dropping "is" and "has"
+    - filtering by elaboration
+    - selection by detected equality groups
+
+## Possible improvements
+
+* Interface: code actions
+* Prompting:
+    - better selection of prompts
+    - better/larger database of prompts
+    - use proximate prompts
+* Post-processing
+    - expand auto-correction
+    - suggest imports
+    - expand equality detection for selection
+    - improve selections: round-trips
+* Codex improvements:
+    - hyper-parameters
+    - fine-tuning
 -/
