0d33ffb86b6128803c2d06e8b7878a7654244414
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-10-07 11:54:47 +0530

    simple dot names

--- LeanCodePrompts/ExploreTranslateExamples.lean
+++ LeanCodePrompts/ExploreTranslateExamples.lean
@@ -14,3 +14,23 @@ def eg3 := "If a set `S` contains `0` and `1`, and the mean of every finite none
 #eval translate eg3 #[("`3` is odd", ": Odd 3")]
 
 #eval showLogs 1
+
+def eg4 := "Every sequence of natural numbers is bounded."
+
+#eval translate eg4
+
+def eg5 := "Every sequence `x_n` of natural numbers is constant."
+
+#eval translate eg5
+
+def eg6 := "Every finite sequence `x_1, x_2, ..., x_n` of natural numbers is bounded."
+
+#eval translate eg6
+
+#eval showLogs 1
+
+def eg7 := "If a set `S` contains `0` and `1`, and the mean of every finite nonempty subset of `S`, then `S` is non-empty."
+
+#eval translate eg7
+
+#eval showLogs 1
