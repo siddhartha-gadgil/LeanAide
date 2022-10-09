a62b0ee9abf750c33ac7be627c2a26d14b4fe5da
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-10-09 08:31:50 +0530

    commented out timing logs

--- LeanCodePrompts/ExploreTranslateExamples.lean
+++ LeanCodePrompts/ExploreTranslateExamples.lean
@@ -13,7 +13,7 @@ def eg2 := "Every set of `10` distinct numbers between `1` and `100` contains tw
 
 def eg3 := "If a set `S` contains `0` and `1`, and the mean of every finite nonempty subset of `S`, then `S` contains all the rational numbers in the unit interval."
 
-#eval translate eg3 #[("`3` is odd", ": Odd 3")]
+#eval translate eg3 
 
 #eval showLogs 1
 
