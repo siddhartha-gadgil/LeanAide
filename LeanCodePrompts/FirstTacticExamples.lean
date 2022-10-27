2a06c0b58c1e1b57dd6f0907d9060726f77144d1
ART0 <18333981+0Art0@users.noreply.github.com>
2022-10-27 15:42:02 +0530

    Import `Mathlib` and add custom tactics

--- LeanCodePrompts/FirstTacticExamples.lean
+++ LeanCodePrompts/FirstTacticExamples.lean
@@ -1,13 +1,20 @@
 import LeanCodePrompts.FirstTacticFinder
+import Mathlib.Tactic.Basic
 -- import Mathbin.All
+
+macro "existsi " e:term : tactic => 
+  `(apply Exists.intro $e)
+
+macro "use " e:term : tactic =>
+  `(apply Exists.intro $e)
 
 
 example : ∀ n : ℕ, ∃ m : ℕ, n < 2 * m + 1 := by 
-  show_tactic_prompt
-  aide
-  aide
-  aide
-
+  -- show_tactic_prompt
+  -- aide
+  -- aide
+  intro n
+  
   repeat (sorry)
 
 -- example : ∀ n : ℕ, ∃ m : ℕ, n < 2 * m + 1 := by
