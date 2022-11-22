07c3eddb190bd65422f02b376dcca3c7a342d999
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2022-11-22 13:11:28 +0530

    silly example testing Using codeActions #5

--- LeanCodePrompts/CodeActions.lean
+++ LeanCodePrompts/CodeActions.lean
@@ -31,3 +31,29 @@ open Lean Server Lsp RequestM
 
   let ca : CodeAction := { title := "Translate theorem docstring to Lean code", kind? := "quickfix" }
   return #[{ eager := ca, lazy? := some $ return {ca with edit? := WorkspaceEdit.ofTextEdit params.textDocument.uri $ ← edit} }]
+
+open RequestM in
+@[codeActionProvider]
+def readFile : CodeActionProvider := fun params _snap => do
+  let doc ← readDoc
+  let text := doc.meta.text
+  let source := text.source
+  let pos := text.lspPosToUtf8Pos params.range.end
+  let edit : TextEdit := {
+    range := params.range
+    newText :=
+      let tail := Substring.mk source pos source.endPos
+      let tail := tail.toString.splitOn "/-" |>.head!
+      "/- " ++ tail ++ "-/"
+  }
+  let ca : CodeAction := {title := "tail of source", kind? := "quickfix"}
+  return #[{eager := ca, lazy? := some $ return { ca with edit? := WorkspaceEdit.ofTextEdit params.textDocument.uri edit}}]
+  
+/- 
+
+example : 1 = 1 := by
+  simp
+-/
+
+example : 1 = 1 := by
+  simp
