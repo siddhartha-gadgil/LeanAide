4614cefadbb6e1c8e5e653c56e83bef89dcf5818
ART0 <18333981+0Art0@users.noreply.github.com>
2022-11-22 17:47:03 +0530

    Add `nearestComment` code

--- LeanCodePrompts/CodeActions.lean
+++ LeanCodePrompts/CodeActions.lean
@@ -3,9 +3,30 @@ import LeanCodePrompts.Translate
 
 open Lean Server Lsp RequestM
 
+partial def String.Iterator.findFirst? : String.Iterator → String → Option String.Pos
+  | i, pre => if i.hasNext then
+    if i.remainingToString.startsWith pre then 
+      some i.pos 
+    else 
+      i.next.findFirst? pre
+  else none
+
+def String.Range.expand : String.Range → Nat → String.Range
+  | ⟨start, stop⟩, i => ⟨start - ⟨i⟩, stop + ⟨i⟩⟩
+
 /-- Finds the comment or doc-string in the source nearest to the given position. -/
-def nearestComment (source : Substring) (pos : String.Pos) : 
-  Option String.Range := sorry
+partial def nearestComment (source : String) 
+  (pos : String.Pos) (start : String.Pos := ⟨0⟩) : Option String.Range := do
+    guard $ start ≤ pos
+    let firstCommentRange ← findFirstComment? source start
+    if (firstCommentRange.expand 2).contains pos then
+      some firstCommentRange
+    else
+      nearestComment source pos (source.next firstCommentRange.stop)
+  where findFirstComment? (source : String) (init : String.Pos) : Option String.Range := do
+    let start ← (String.Iterator.mk source init).findFirst? "/-"
+    let stop ← (String.Iterator.mk source start).findFirst? "-/"
+    return ⟨start, stop + ⟨2⟩⟩
 
 /-- Extracts the text contained in a comment. -/
 def extractCommentText (comment : String) : Option String :=
@@ -22,9 +43,6 @@ def Syntax.extractComment : Syntax → Option String
 #check getDocStringText
 -/
 
-#check String.anyAux
-#check String.nextUntil
-
 /-- A code action for translating doc-strings to Lean code using OpenAI Codex -/
 @[codeActionProvider] def formaliseDocStr : CodeActionProvider := fun params snap => do
   let doc ← readDoc
@@ -32,35 +50,29 @@ def Syntax.extractComment : Syntax → Option String
   let source := text.source
 
   let edit : IO TextEdit := return {
-    range :=
-      -- the end of the current snapshot
-      let endPos := text.leanPosToLspPos <| text.toPosition snap.endPos
-      -- the trivial range starting and ending at the last position in the current snapshot
-      ⟨endPos, endPos⟩
+    range := params.range
     newText := ← do
       -- the current position in the text document
-      let currentPos : String.Pos := text.lspPosToUtf8Pos params.range.end
+      let pos : String.Pos := text.lspPosToUtf8Pos params.range.end
       -- the smallest node of the `InfoTree` containing the current position
-      let info? := snap.infoTree.findInfo? (·.contains currentPos)
+      let info? := snap.infoTree.findInfo? (·.contains pos)
       -- the `Syntax` corresponding to the `Info` node
       let stx? := (·.stx) <$> info?
 
       -- the statement to be translated to Lean code
       let stmt? : Option String := 
         (none /- TODO: First attempt to parse using `Syntax` -/)  <|>
-        (do
-          let snapWindow := Substring.mk source snap.beginPos snap.endPos
-          let ⟨start, «end»⟩ ← nearestComment snapWindow currentPos
+        (do /- Parse as a string -/
+          let ⟨start, «end»⟩ ← nearestComment source pos
           let comment := source.extract start «end»
           extractCommentText comment
         ) <|> 
         (some "Every prime number is odd.")
 
-      match stmt? with
-        | some stmt => 
-          let translation := snap.runTermElabM doc.meta <| translateViewM stmt
-          EIO.toIO (λ _ => IO.userError "Translation failed.") translation
-        | none => throw $ IO.userError "No input found."
+      let some stmt := stmt? | throw $ IO.userError "No input found."
+      let translation : IO String := return stmt
+      -- let translation := snap.runTermElabM doc.meta <| translateViewM stmt
+      EIO.toIO (λ _ => IO.userError "Translation failed.") translation
   }
 
   let ca : CodeAction := { title := "Translate theorem docstring to Lean code", kind? := "quickfix" }
@@ -88,6 +100,3 @@ def readFile : CodeActionProvider := fun params _snap => do
 example : 1 = 1 := by
   simp
 -/
-
-example : 1 = 1 := by
-  simp
