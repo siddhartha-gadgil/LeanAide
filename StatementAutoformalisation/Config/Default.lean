74dd73f8cb817fb7a648285406014baef644ee74
ART0 <18333981+0Art0@users.noreply.github.com>
2022-12-06 18:38:17 +0530

    Add alternative configurations

---
+++ StatementAutoformalisation/Config/Default.lean
@@ -0,0 +1,39 @@
+import StatementAutoformalisation.Translate
+import StatementAutoformalisation.Config.FixedPrompts
+
+def LLMParams : LLM.Params :=
+{
+  model := "code-davinci-002",
+  temperature := 8,
+  n := 7,
+  maxTokens := 300,
+  stopTokens := #[":=", "\n\n/-", "\n/-", "/-"]
+}
+
+def SentenceSimilarityParams : SentenceSimilarity.Params :=
+{
+  source := "data/safe-prompts.json",
+  model := "all-mpnet-base-v2",
+  kind := "theorem",
+  field := "doc_string",
+  nSim := 15
+}
+
+def KeywordExtractionParams : KeywordExtraction.Params :=
+{
+  nKw := 0
+}
+
+def CustomPromptParams : Prompt.Params :=
+{
+  LLMParams, 
+  SentenceSimilarityParams, 
+  KeywordExtractionParams with
+  fixedPrompts := fixedPrompts,
+  useNames := #[],
+  useModules := #[],
+  useMainCtx? := false,
+  printDecl := DeclarationWithDocstring.toString
+  mkSuffix := fun stmt => s!"{printAsComment stmt}\n{SentenceSimilarityParams.kind}",
+  processCompletion := id
+}
---
+++ StatementAutoformalisation/Config/ReverseTranslation.lean
@@ -0,0 +1,39 @@
+import StatementAutoformalisation.Translate
+import StatementAutoformalisation.Config.FixedPrompts
+
+def LLMParams : LLM.Params :=
+{
+  model := "code-davinci-002",
+  temperature := 2,
+  n := 1,
+  maxTokens := 200,
+  stopTokens := #["\n\n"]
+}
+
+def SentenceSimilarityParams : SentenceSimilarity.Params :=
+{
+  source := "data/safe-prompts.json",
+  model := "all-mpnet-base-v2",
+  kind := "theorem",
+  field := "doc_string",
+  nSim := 0
+}
+
+def KeywordExtractionParams : KeywordExtraction.Params :=
+{
+  nKw := 0
+}
+
+def CustomPromptParams : Prompt.Params :=
+{
+  LLMParams, 
+  SentenceSimilarityParams, 
+  KeywordExtractionParams with
+  fixedPrompts := fixedPrompts,
+  useNames := #[],
+  useModules := #[],
+  useMainCtx? := false,
+  printDecl := { toString := fun ⟨decl, docstring⟩ => s!"{toString decl}\n{printAsComment docstring}" }
+  mkSuffix := fun stmt => s!"{stmt}\n/--",
+  processCompletion := id
+}
