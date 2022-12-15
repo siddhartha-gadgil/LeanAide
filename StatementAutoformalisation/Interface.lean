59b836164b708175381d3d2b6971adaca374b061
ART0 <18333981+0Art0@users.noreply.github.com>
2022-12-15 12:28:24 +0530

    Abstract interface to handle syntax processing

--- StatementAutoformalisation/Interface.lean
+++ StatementAutoformalisation/Interface.lean
@@ -6,13 +6,13 @@ structure Interface.Params (α : Type _) where
   /-- The title of the code action. -/
   title : String
   /-- The occurrence of the desired pattern that is closest to the given position. -/
-  nearestOccurrence? : (source : String) → (pos : String.Pos) → Option String.Range
+  nearestOccurrence? : (source : String) → (pos : String.Pos) → Option String.Range := fun _ _ => none
   /-- Process the selected portion of the file to extract the main text of interest. -/
   extractText? : String → Option String
   /-- The transformation to perform to the input text (for example, translation). -/
   action : String → Lean.Elab.TermElabM α
   /-- Format and render the final output. -/
-  postProcess : (input : Option String) → (output : α) → String
+  postProcess : (input : String) → (output : α) → String
 
 open Lean Server Lsp RequestM in
 /-- An abstraction for performing edits through code actions. -/
@@ -24,33 +24,39 @@ def performCodeAction {T : Type _} (iparams : Interface.Params T) : CodeActionPr
   let edit : IO TextEdit := do
     -- the current position in the text document
     let pos : String.Pos := text.lspPosToUtf8Pos params.range.end
-    let some ⟨start, stop⟩ := iparams.nearestOccurrence? source pos | throw $ IO.userError "No input found."
-    return {
-    range := ⟨text.leanPosToLspPos <| text.toPosition start, text.leanPosToLspPos <| text.toPosition stop⟩
-    newText := ← do
-      -- the smallest node of the `InfoTree` containing the current position
-      let info? := _snap.infoTree.findInfo? (·.contains pos)
-      -- the `Syntax` corresponding to the `Info` node
-      let stx? := (·.stx) <$> info?
 
-      -- the portion of the document to be processed
-      let rawText? : Option String :=
-        -- this is safe because the `range` is available
-        (none /- TODO: First attempt to parse using `Syntax` -/)  <|>
-        ( /- Parse as a string -/
-          some <| source.extract start stop)
-      let input? := rawText? >>= iparams.extractText?
+    -- the portion of the document to be processed
+    let input? : Option (String × String.Range) :=
+        ( do /- Parse the `Syntax` using the `InfoTree` -/
+          -- the smallest node of the `InfoTree` containing the current position
+          let info ← _snap.infoTree.findInfo? (·.contains pos)
+          -- the `Syntax` corresponding to the `Info` node
+          let stx := info.stx
+          let rawText ← stx.reprint
+          pure (← iparams.extractText? rawText, ← info.range?) )
+           <|> /- alternatively -/
+        ( do /- Parse the document as a string -/
+          let range ← iparams.nearestOccurrence? source pos
+          let rawText := source.extract range.start range.stop
+          pure (← iparams.extractText? rawText, range) )
 
-      let output := _snap.runTermElabM doc.meta <| do
-        let some input := input? | failure
-        iparams.action input
-      let output ← EIO.toIO (fun _ => IO.userError "Action failed.") output
-      return iparams.postProcess input? output
-    }
+    match input? with
+      | some (txt, ⟨start, stop⟩) => return {
+          range :=
+            ⟨text.leanPosToLspPos <| text.toPosition start, 
+            text.leanPosToLspPos <| text.toPosition stop⟩
+          newText := ← do
+            let output ← EIO.toIO (fun _ => IO.userError "Action failed.") <|
+                _snap.runTermElabM doc.meta <| iparams.action txt
+            return iparams.postProcess txt output }
+      | none => throw <| IO.userError "Parsing input failed."
 
   let ca : CodeAction := { title := iparams.title, kind? := "quickfix" }
-  return #[{ eager := ca, lazy? := some $ return {ca with edit? := WorkspaceEdit.ofTextEdit params.textDocument.uri $ ← edit} }]
-
+  return #[{ 
+    eager := ca, 
+    lazy? := some $ return { ca with 
+      edit? := WorkspaceEdit.ofTextEdit params.textDocument.uri $ ← edit} 
+    }]
 
 partial def String.Iterator.findFirst? : String.Iterator → String → Option String.Pos
   | i, pre => if i.hasNext then
