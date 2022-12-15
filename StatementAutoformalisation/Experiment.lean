8e6f41151a0be1867c7c9e361e3c5ad1ae8a34a0
ART0 <18333981+0Art0@users.noreply.github.com>
2022-12-15 18:29:49 +0530

    Add docs for experiment file

--- StatementAutoformalisation/Experiment.lean
+++ StatementAutoformalisation/Experiment.lean
@@ -4,6 +4,18 @@ import StatementAutoformalisation.Config.Informalisation
 import StatementAutoformalisation.Config.SuggestName
 -- import Mathbin.All
 
-/- Every natural number can be written as the sum of two squares. -/
+/-!
+The code actions implemented so far can be tested here.
+
+- To test autoformalisation, click anywhere on the comment below and 
+  select the code action to translate it to Lean code.
+- To test informalisation, click anywhere on the `example` below (EXCEPT 
+  the end) and attempt to generate a natural language description.
+- To test name suggestions, select the *type* of the `example` below
+  (everything between `example` and `:=`, including the first `:`) and 
+  attempt to generate a name for the declaration.
+-/
+
+/- Every prime number is either `2` or odd. -/
 
 example : ∀ n : Nat, ∃ m : Nat, m > n ∧ m % 2 = 1  := sorry
