9fd5e16077913d47c205f15055f54ff4a21c78f0
ART0 <18333981+0Art0@users.noreply.github.com>
2022-12-27 12:53:08 +0530

    A working `showTerm` tactic

--- CodeAction/ShowTerm.lean
+++ CodeAction/ShowTerm.lean
@@ -1,1 +1,32 @@
 import CodeAction.Interface
+
+open Lean Meta Elab Parser Tactic
+
+def tacticSeq.showTerm (tacSeq : TSyntax ``tacticSeq) : TacticM Unit := do
+  let mvar ← getMainGoal
+  evalTacticSeq tacSeq
+  let some term ← getExprMVarAssignment? mvar | failure
+  let term ← instantiateMVars term
+  addTrace `test m!"{← PrettyPrinter.ppExpr term}"
+
+elab "showTerm " t:tacticSeq : tactic =>
+  tacticSeq.showTerm t
+
+section Test
+
+example : True := by
+  showTerm
+    trivial
+
+example : true := by
+  showTerm
+    rfl
+
+example : ∀ n : Nat, n + .zero = .zero + n := by
+  intro n
+  showTerm
+    rw [Nat.add_zero]
+  showTerm
+    rw [Nat.zero_add]
+
+end Test
