621bee187ab8e7b1765d0a1fd027c2ee265670be
ART0 <18333981+0Art0@users.noreply.github.com>
2023-01-09 14:12:31 +0530

    Remove command line arguments

--- promptsgen.lean
+++ promptsgen.lean
@@ -47,7 +47,7 @@ def forbiddenPrefixes : Array Name :=
 
 def blacklistFiles := [`StatementAutoformalisation.Declaration]
 
-def outputFile : System.FilePath := "data/mathlib4-prompts"
+def outputFile : System.FilePath := "data/mathlib4-prompts.json"
 
 open Lean in
 def generatePrompts : MetaM Unit := do
@@ -69,9 +69,9 @@ def generatePrompts : MetaM Unit := do
   liftM <| IO.FS.writeFile outputFile (Json.arr data).pretty
 
 open Lean
-def main (args : List String) : IO Unit := do
+def main : IO Unit := do
   IO.println "Generating prompts..."
   initSearchPath (← Lean.findSysroot) ["lake-packages/mathlib/build/lib/",  "lake-packages/std/build/lib/", "lake-packages/Qq/build/lib/", "lake-packages/aesop/build/lib/"]
-  let env ← importModules [{module := .mkStr `Mathlib args.head!}] {}
+  let env ← importModules [{module := `Mathlib}] {}
   Prod.fst <$> generatePrompts.toIO {fileName := "", fileMap := default, maxHeartbeats := 5000000} {env := env}
-  IO.println s!"Output written to {outputFile}-{args.head!}.json."
+  IO.println s!"Output written to {outputFile}."
