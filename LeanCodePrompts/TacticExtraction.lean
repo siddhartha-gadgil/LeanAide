6a8cb0f92eb2e94ee142baacd7b2110e72a3d40e
0art0 <18333981+0art0@users.noreply.github.com>
2023-02-12 12:38:18 +0530

    Add functions for tactic data extraction

--- LeanCodePrompts/TacticExtraction.lean
+++ LeanCodePrompts/TacticExtraction.lean
@@ -4,12 +4,14 @@ import LeanInk.Analysis.Basic
 open Lean
 
 
-def inputFile : System.FilePath := "lake-packages"/"mathlib"/"Mathlib"/"Data"/"Int"/"Basic.lean"  
+def inputFile : System.FilePath := "lake-packages"/"mathlib"/"Mathlib"/"Data"/"Int"/"Dvd"/"Pow.lean"
+
+#eval inputFile.pathExists
 
 def tacticExtractionConfig : IO LeanInk.Configuration := return {
   inputFilePath := inputFile
   inputFileContents := ← IO.FS.readFile inputFile
-  lakeFile := some ("lake-packages"/"leanInk"/"lakefile.lean")
+  lakeFile := some $ "lake-packages"/"leanInk"/"lakefile.lean"
   verbose := true
   prettifyOutput := true
   experimentalTypeInfo := true
@@ -17,3 +19,14 @@ def tacticExtractionConfig : IO LeanInk.Configuration := return {
   experimentalSemanticType := false
 }
 
+def tacticData : IO <| List LeanInk.Analysis.Sentence := do
+  let config ← tacticExtractionConfig
+  let result ← LeanInk.Analysis.analyzeInput.run config 
+  return result.sentences
+
+def tacticDataStrings : IO <| List String := do
+  let tacs ← tacticData
+  return tacs.map toString 
+  -- TODO replace the default `toString` instance with a more descriptive one
+
+#eval tacticDataStrings
