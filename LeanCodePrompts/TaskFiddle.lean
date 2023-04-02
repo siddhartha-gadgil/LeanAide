e090d0d0e6e8274a9fb0a8067414ad399cd69f19
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-04-02 20:33:21 +0530

    trimmed, added "check"s to TaskFiddle

--- LeanCodePrompts/TaskFiddle.lean
+++ LeanCodePrompts/TaskFiddle.lean
@@ -4,46 +4,6 @@ open Lean Meta Elab Tactic Parser
 
 #eval 3
 
-#check IO.asTask
-
--- From Lean 4 source
-
--- #eval id (α := IO _) do
---   let t1 ← IO.asTask $ Nat.forM 10 fun _ => IO.println "hi";
---   let t2 ← IO.asTask $ Nat.forM 10 fun _ => IO.println "ho";
---   IO.ofExcept t1.get
-
--- #eval id (α := IO _) do
---   let t1 ← IO.mapTask IO.println (Task.spawn fun _ => "ha");
---   pure ()
-
--- #eval id (α := IO _) do
---   let t1 ← IO.bindTask (Task.spawn fun _ => "hu") fun s =>
---     IO.asTask (IO.println s);
---   pure ()
-
--- #eval id (α := IO _) do
---   let t1 ← IO.asTask do {
---     let c ← IO.checkCanceled;
---     IO.println (if c then "canceled!" else "done!")
---   };
---   pure ()
-
--- #eval id (α := IO _) do
---   let t1 ← IO.asTask do {
---     let c ← IO.checkCanceled;
---     IO.println (if c then "canceled! 2" else "done! 2")
---   };
---   IO.cancel t1;
---   discard $ IO.wait t1;
---   pure ()
-
-#eval IO.waitAny [
-  Task.spawn fun _ => dbgSleep 2 fun _ => "A",
-  Task.spawn fun _ => dbgSleep 3 fun _ => "B",
-  Task.spawn fun _ => dbgSleep 1 fun _ => "C"
-]
-
 def slowFibIO : Nat → IO Nat
 | 0 => pure 0
 | 1 => pure 1
@@ -59,3 +19,21 @@ example : 5 = 5 := by
   run_io_task
   rfl
 
+#check Core.CoreM.run -- {α : Type} → CoreM α → Core.Context → Core.State → EIO Exception (α × Core.State)
+#check Meta.MetaM.run /-{α : Type} →
+  MetaM α →
+    optParam Meta.Context
+        {...} →
+      optParam Meta.State
+          {...} →
+        CoreM (α × Meta.State)
+-/
+#check Elab.runTactic /- MVarId →
+  Syntax →
+    optParam Term.Context
+        {...} →
+      optParam Term.State
+          { levelNames := [], syntheticMVars := ∅, pendingMVars := ∅, mvarErrorInfos := ∅, letRecsToLift := [] } →
+        MetaM (List MVarId × Term.State)-/
+#check IO.asTask /- {α : Type} → IO α → optParam Task.Priority Task.Priority.default → BaseIO (Task (Except IO.Error α)) -/
+#check evalTactic
