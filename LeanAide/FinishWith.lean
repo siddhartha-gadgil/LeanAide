a11b8ffd9cd60198c368226bf384fa3f75127307
ART0 <18333981+0Art0@users.noreply.github.com>
2023-07-25 16:46:17 +0530

    Example async code action

--- LeanAide/FinishWith.lean
+++ LeanAide/FinishWith.lean
@@ -1,40 +1,61 @@
 import Lean
-import Std.Tactic.TryThis
+import Std
 
 open Lean Elab Tactic Parser Tactic Term
 open Lsp Server RequestM
-open Std Tactic
+open Std Tactic CodeAction
 
 def Lean.Elab.GoalsAtResult.display (goalsAt : Lean.Elab.GoalsAtResult) : IO (Format × Format) := do
   let goalsBefore ← goalsAt.ctxInfo.ppGoals goalsAt.tacticInfo.goalsBefore
   let goalsAfter ← goalsAt.ctxInfo.ppGoals goalsAt.tacticInfo.goalsAfter
   return (goalsBefore, goalsAfter)
 
-syntax (name := finishTac) "finish_with" tacticSeqBracketed tacticSeq : tactic
-
-@[tactic finishTac]
-def finishTacElab : Tactic
-  | stx@`(tactic| finish_with $cmd $[$tacs]*) => do
-    for tac in tacs do
-      try
-        evalTactic cmd
-        unless (← getGoals).isEmpty do
-          throwError m!"Unfinished goals."
-        logInfoAt tac m!"Goals closed with {cmd}."
-      catch e => continue
-      evalTactic tac
-  |  _ => throwUnsupportedSyntax
-
--- A dummy version of `by#` to test `Try this ...` replacement
 syntax (name := bySharpTac) "by#" tacticSeq : term
 
 @[term_elab bySharpTac] def bySharpElab : TermElab
   | stx@`(term| by# $tacs), type? => do
     let by_block ← `(term| by $tacs)
-    TryThis.addSuggestion stx by_block
+    -- TryThis.addSuggestion stx by_block
     elabTerm by_block type?
   | _, _ => throwUnsupportedSyntax
 
-example : ∀ n : ℕ, n = n := by#
-  intro n
+@[tactic_code_action*]
+def testTacticCodeAction : TacticCodeAction := fun params snap ci stk node ↦ do
+  let .node (.ofTacticInfo info) _ := node | return #[]
+  let .some (tacs, _) := stk[stk.length - 4]? | return #[]
+  -- ensures that the tactic is within an `async` block
+  unless (Syntax.getKind <$> tacs.getHead?) = some `«by#» do
+    return #[]
+  
+  let some start := tacs.getPos? | return #[]
+  let some stop := tacs.getTailPos? | return #[]
+
+  let doc ← readDoc
+  let eager := {
+    title := "Clear tactic block."
+    kind? := "quickfix"
+    isPreferred? := true
+    edit? := some <|.ofTextEdit params.textDocument.uri {
+      range := doc.meta.text.utf8RangeToLspRange ⟨start, stop⟩
+      newText := "sorry"
+    }
+  }
+  pure #[{ eager }]
+
+  
+
+
+theorem xyz : ∀ n : ℕ, n = n := by#
+  skip
+  simp
+
+example : 1 = 1 ↔ 2 = 2 := by#
+  constructor
+  · intro
+    rfl
+  · intro
+    rfl  
+
+-- The code action does not appear in ordinary `by` blocks, by design
+example : 1 = 1 := by
   rfl
