d0146d13d489e161694a45d0b322343b889f18ab
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-07-27 10:48:15 +0530

    guard messages for tests

--- LeanAide/TestAsync.lean
+++ LeanAide/TestAsync.lean
@@ -7,23 +7,55 @@ opaque sillyN : Nat
 
 axiom silly : sillyN = 2
 
+/--
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
 example : sillyN ≤ 3 := by#
   sorry
 
+/--
+info: Try this: by simp_all only
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
 example : 3 ≤ 4 := by#
   sorry
 
 example : sillyN ≤ 4 := by#
   rw [silly]
   sorry
-  
+
+/--
+info: Try this: by simp_all only
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in  
 example : 2 ≤ 2 := by#
   sorry
 
+/--
+info: Try this: simp_all only
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
 example : 2 ≤ 2 := by
   aided_by aesop? do
   sorry
 
+/--
+info: Try this: by simp_all only
+---
+info: Try this: by
+  skip
+  simp_all only
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
 example : 2 ≤ 2 := by#
    skip
    sorry
@@ -41,6 +73,12 @@ example : 2 ≤ 3 := by apply?
 
 set_option aided_by.delay 200
 
+/--
+info: Try this: by exact Nat.le_of_ble_eq_true rfl
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
 example : 2 ≤ 4 := by!
   sorry
 
@@ -54,7 +92,14 @@ example : prop := by
 example : 1 = 1 := by!
   sorry
 
-
+/--
+info: Try this: by
+  rw [silly]
+  simp_all only
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
 example : sillyN ≤ 4 := by#
   rw [silly]
   sorry
@@ -72,6 +117,14 @@ def sum : ℕ → ℕ
 
 set_option aided_by.delay 600
 
+/--
+info: Try this: 
+  simp [sum]
+  linarith
+---
+warning: declaration uses 'sorry'
+-/
+#guard_msgs in
 theorem sum_formula (n: ℕ) :  sum n = (n * (n + 1) : ℚ) / 2  := by 
   induction n with
   | zero => rfl
