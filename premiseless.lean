8251067d658627d59f924a939bfadc607d78b370
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-08-02 12:04:45 +0530

    increasing heartbeats

--- premiseless.lean
+++ premiseless.lean
@@ -10,7 +10,7 @@ def environment : IO Environment := do
     {module := `Mathlib}] {}
 
 def coreContext : Core.Context := 
-  {fileName := "", fileMap := ⟨"", #[], #[]⟩, maxHeartbeats := 1000000000, maxRecDepth := 100000, openDecls := [Lean.OpenDecl.simple `LeanAide.Meta []]
+  {fileName := "", fileMap := ⟨"", #[], #[]⟩, maxHeartbeats := 10000000000, maxRecDepth := 1000000, openDecls := [Lean.OpenDecl.simple `LeanAide.Meta []]
     }   
 
 def main (_: List String) : IO Unit := do
