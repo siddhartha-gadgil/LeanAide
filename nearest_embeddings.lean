6621a96f0236fdb3e9aa780fe138bc53d9d09bb4
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-08-08 18:30:32 +0530

    restored working nearest_embedding

--- nearest_embeddings.lean
+++ nearest_embeddings.lean
@@ -1,27 +1,17 @@
-import Lean
+import LeanCodePrompts.Embeddings
+import LeanAide.Aides
+import Lean.Data.Json
+import Mathlib.Util.Pickle
 open Lean
 
--- A dummy function
-def nearestDocsToDoc' (doc : String) (k : Nat) : 
-  IO (Array (String × String)) := pure #[
-    ("/-- Fermat's Last theorem -/", "∀ x y z n : ℕ, n > 2 → x^ + y^n = z^n → x*y*z=0"),
-    ("/-- There are infinitely many odd numbers -/", "∀ n : ℕ, ∃ m : ℕ, m > n ∧ Odd m")
-  ]
+unsafe def show_nearest (stdin stdout : IO.FS.Stream)(data: Array (String × FloatArray)) : IO Unit := do
+  let doc ← stdin.getLine
+  let embs ← nearestDocsToDoc data doc 10
+  let out := Lean.Json.arr <| embs.toArray.map Json.str
+  stdout.putStrLn out.compress
+  show_nearest stdin stdout data
 
-unsafe def main (args : List String) : IO Unit := do
-  let [arg] := args | IO.throwServerError "Expected exactly one argument."
-  let k := arg.toNat!
-
+unsafe def main (_: List String) : IO Unit := do
   let stdin ← IO.getStdin
   let stdout ← IO.getStdout
-
-  stdout.putStrLn "Nearest `mathlib` embeddings\n"
-  showNearestDocs stdin stdout k
-
-where  
-  showNearestDocs (stdin stdout : IO.FS.Stream) (k : Nat) : IO Unit := do
-    let doc ← stdin.getLine
-    let embs ← nearestDocsToDoc' doc k
-    let out : Lean.Json := .arr <| embs.map fun (docstr, thm) ↦ .mkObj [("docstring", docstr), ("theorem", thm)]
-    stdout.putStrLn out.compress
-    showNearestDocs stdin stdout k
+  withUnpickle  "rawdata/mathlib4-thms-embeddings.olean" <| fun (data : Array <| String ×  FloatArray) => show_nearest stdin stdout data
