927fc69d3618be34f7c3c0d211ef1ad9db90776d
ART0 <18333981+0Art0@users.noreply.github.com>
2023-08-08 16:52:36 +0530

    Remove picking for debugging

--- nearest_embeddings.lean
+++ nearest_embeddings.lean
@@ -1,11 +1,8 @@
-import LeanCodePrompts.Embeddings
-import LeanAide.Aides
-import Lean.Data.Json
-import Mathlib.Util.Pickle
+import Lean
 open Lean
 
 -- A dummy function
-def nearestDocsToDoc' (data : Array (String × FloatArray)) (doc : String) (k : Nat) : 
+def nearestDocsToDoc' (doc : String) (k : Nat) : 
   IO (Array (String × String)) := pure #[
     ("/-- Fermat's Last theorem -/", "∀ x y z n : ℕ, n > 2 → x^ + y^n = z^n → x*y*z=0"),
     ("/-- There are infinitely many odd numbers -/", "∀ n : ℕ, ∃ m : ℕ, m > n ∧ Odd m")
@@ -18,25 +15,13 @@ unsafe def main (args : List String) : IO Unit := do
   let stdin ← IO.getStdin
   let stdout ← IO.getStdout
 
-  unless ← picklePath.pathExists do
-    IO.println "Fetching embeddings ..."
-    let out ← IO.Process.run {
-      cmd := "curl",
-      args := #["-O", "https://math.iisc.ac.in/~gadgil/data/mathlib4-thms-embeddings.olean"],
-      cwd := "./rawdata/"
-    }
-    IO.println out
+  stdout.putStrLn "Nearest `mathlib` embeddings\n"
+  showNearestDocs stdin stdout k
 
-  withUnpickle picklePath <| fun data ↦ do
-    stdout.putStrLn "Nearest `mathlib` embeddings\n"
-    showNearestDocs stdin stdout k data
-
-where
-  picklePath : System.FilePath := "./rawdata/mathlib4-thms-embeddings.olean"
-  
-  showNearestDocs (stdin stdout : IO.FS.Stream) (k : ℕ) (data : Array (String × FloatArray)): IO Unit := do
+where  
+  showNearestDocs (stdin stdout : IO.FS.Stream) (k : Nat) : IO Unit := do
     let doc ← stdin.getLine
-    let embs ← nearestDocsToDoc' data doc k
+    let embs ← nearestDocsToDoc' doc k
     let out : Lean.Json := .arr <| embs.map fun (docstr, thm) ↦ .mkObj [("docstring", docstr), ("theorem", thm)]
     stdout.putStrLn out.compress
-    showNearestDocs stdin stdout k data
+    showNearestDocs stdin stdout k
