afa2d7759c1ec1f8f6be895b2827151123d02e24
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-08-09 08:54:56 +0530

    allowing halt

--- nearest_embeddings.lean
+++ nearest_embeddings.lean
@@ -6,18 +6,21 @@ open Lean
 
 unsafe def show_nearest (stdin stdout : IO.FS.Stream)(data: Array (String × FloatArray)) : IO Unit := do
   let inp ← stdin.getLine
-  let (doc, num) := 
+  let (doc, num, halt) := 
     match Json.parse inp with
     | Except.error _ => (inp, 10)
     | Except.ok j => 
       (j.getObjValAs? String "docString" |>.toOption.orElse 
         (fun _ => j.getObjValAs? String "doc_string" |>.toOption) 
         |>.getD inp, 
-      j.getObjValAs? Nat "n" |>.toOption.getD 10)
+      j.getObjValAs? Nat "n" |>.toOption.getD 10,
+      j.getObjValAs? Bool "halt" |>.toOption.getD false)
   let embs ← nearestDocsToDoc data doc num
   let out := Lean.Json.arr <| embs.toArray.map Json.str
   stdout.putStrLn out.compress
-  show_nearest stdin stdout data
+  unless halt do 
+    show_nearest stdin stdout data
+  return ()
 
 unsafe def main (_: List String) : IO Unit := do
   let stdin ← IO.getStdin
