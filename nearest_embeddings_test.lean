9576ae1b87cdcbe50600328e0e121c54d769fd31
ART0 <18333981+0Art0@users.noreply.github.com>
2023-08-12 14:17:59 +0530

    Storing the process child in a `Ref`

--- nearest_embeddings_test.lean
+++ nearest_embeddings_test.lean
@@ -1,23 +1,34 @@
 import Lean
+import Mathlib.Tactic
 open Lean
 
 def nearestEmbeddingsCmd : IO.Process.SpawnArgs := {
   cmd := "lake",
   args := #["exe", "nearest_embeddings"],
-  cwd := "."
+  cwd := ".",
+  stdin := .piped,
+  stdout := .piped,
+  stderr := .piped
 }
 
-def statements := #[
-  "There are infinitely many odd numbers",
-  "Every vector space of dimension 2 is finite dimensional",
-  "Every subgroup of an Abelian group is Abelian"]
 
-def queryProcess (args : IO.Process.SpawnArgs) (queries : Array String) : IO (Array String) := do
-  IO.println "Starting ..."
-  let child ← IO.Process.spawn { args with
-    stdin := .piped,
-    stdout := .piped,
-    stderr := .piped }
+@[instance] axiom IO.FS.Handle.instInhabited : Inhabited IO.FS.Handle
+
+noncomputable instance (stdio : IO.Process.Stdio) : Inhabited stdio.toHandleType :=
+  match stdio with
+    | .inherit | .null => inferInstanceAs <| Inhabited Unit
+    | .piped => inferInstanceAs <| Inhabited IO.FS.Handle
+
+noncomputable instance : Inhabited (IO.Process.Child nearestEmbeddingsCmd.toStdioConfig) where
+  default := ⟨default, default, default⟩
+
+initialize nearestEmbeddingsProcessRef : IO.Ref (IO.Process.Child nearestEmbeddingsCmd.toStdioConfig) ← do
+  let child ← IO.Process.spawn nearestEmbeddingsCmd
+  IO.mkRef child
+
+
+def queryNearestEmbeddingsProcess (queries : Array String) : IO (Array String) := do
+  let child ← nearestEmbeddingsProcessRef.get 
   let stdin := child.stdin
   let mut outputs : Array String := #[]
   for query in queries do
@@ -27,6 +38,12 @@ def queryProcess (args : IO.Process.SpawnArgs) (queries : Array String) : IO (Ar
     outputs := outputs.push out
   return outputs
 
+
+def statements := #[
+  "There are infinitely many odd numbers",
+  "Every vector space of dimension 2 is finite dimensional",
+  "Every subgroup of an Abelian group is Abelian"]
+
 def main : IO Unit := timeit "nearest_embeddings_test" do
-  let results ← queryProcess nearestEmbeddingsCmd statements
+  let results ← queryNearestEmbeddingsProcess statements
   IO.println results
