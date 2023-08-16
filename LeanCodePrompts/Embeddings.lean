e5cf56dbb1e787e078893811c10a9d60b535f4d6
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-08-16 08:26:42 +0530

    embeddings with prop, should split creation & use

--- LeanCodePrompts/Embeddings.lean
+++ LeanCodePrompts/Embeddings.lean
@@ -7,31 +7,6 @@ import Mathlib
 
 open Std Lean
 
-initialize embeddingsRef  : IO.Ref (Array <| String ×  FloatArray)
-        ← IO.mkRef #[]
-
-def readEmbeddings : IO <| Std.HashMap String FloatArray :=  do
-  let mut count := 0
-  let blob ← IO.FS.readFile "rawdata/mathlib4-thms-embeddings.json"
-  let json := Json.parse blob |>.toOption.get!
-  let jsonArr := json.getArr? |>.toOption.get!
-  let mut accum := Std.HashMap.empty
-  for jsLine in jsonArr do
-    let doc := 
-      match jsLine.getObjValAs? String "docString" with
-      | Except.ok doc => doc
-      | Except.error err => panic! s!"error reading docString: {err}" 
-    let embedding':= 
-      match jsLine.getObjValAs? (List Float) "embedding" with
-      | Except.ok embedding => embedding
-      | Except.error err => panic! s!"error reading embedding: {err}" 
-    let embedding := embedding'.toFloatArray
-    accum := accum.insert doc embedding
-    count := count + 1
-    if count % 1000 == 0 then    
-      IO.println s!"read {count} embeddings"
-  return accum
-
 def readEmbeddingsArray : IO <| Array <| String ×  FloatArray :=  do
   let mut count := 0
   let blob ← IO.FS.readFile "rawdata/mathlib4-thms-embeddings.json"
@@ -57,7 +32,7 @@ def readEmbeddingsArray : IO <| Array <| String ×  FloatArray :=  do
       IO.println s!"read {count} embeddings"
   return accum
 
-def readEmbeddingsFullArray : IO <| Array <| (String × String) ×  FloatArray :=  do
+def readEmbeddingsDocsArray : IO <| Array <| (String × String) ×  FloatArray :=  do
   let mut count := 0
   let blob ← IO.FS.readFile "rawdata/mathlib4-thms-embeddings.json"
   let json := Json.parse blob |>.toOption.get!
@@ -86,15 +61,50 @@ def readEmbeddingsFullArray : IO <| Array <| (String × String) ×  FloatArray :
       IO.println s!"read {count} embeddings"
   return accum
 
+def readEmbeddingsFullDocsArray : IO <| Array <| (String × String × Bool) ×  FloatArray :=  do
+  let mut count := 0
+  let blob ← IO.FS.readFile "rawdata/mathlib4-prompts-embeddings.json"
+  let json := Json.parse blob |>.toOption.get!
+  let jsonArr := json.getArr? |>.toOption.get!
+  let mut accum := #[]
+  let mut docs : Array String := #[]
+  for jsLine in jsonArr do
+    let doc := 
+      match jsLine.getObjValAs? String "docString" with
+      | Except.ok doc => doc
+      | Except.error err => panic! s!"error reading docString: {err}" 
+    let thm :=
+      match jsLine.getObjValAs? String "type" with
+      | Except.ok thm => thm
+      | Except.error err => panic! s!"error reading thmString: {err}"
+    let isProp :=
+      match jsLine.getObjValAs? Bool "isProp" with
+      | Except.ok isProp => isProp
+      | Except.error err => panic! s!"error reading isProp: {err}"
+    let embedding':= 
+      match jsLine.getObjValAs? (List Float) "embedding" with
+      | Except.ok embedding => embedding
+      | Except.error err => panic! s!"error reading embedding: {err}" 
+    let embedding := embedding'.toFloatArray
+    unless docs.contains doc do
+      docs := docs.push doc
+      accum := accum.push ((doc, thm, isProp), embedding)
+    count := count + 1
+    if count % 1000 == 0 then    
+      IO.println s!"read {count} embeddings"
+  return accum
+
 
 unsafe def loadEmbeddingsTest : IO Nat := 
-  withUnpickle  "rawdata/mathlib4-thms-embeddings.olean" <| fun (data: Array <| String ×  FloatArray) => pure data.size
+  withUnpickle  "build/lib/mathlib4-thms-embeddings.olean" <| fun (data: Array <| String ×  FloatArray) => pure data.size
 
-unsafe def loadEmbeddingsFullTest : IO Nat := 
-  withUnpickle  "rawdata/mathlib4-doc-thms-embeddings.olean" <| fun (data: Array <| (String × String) ×  FloatArray) => pure data.size
+unsafe def loadEmbeddingsDocsTest : IO Nat := 
+  withUnpickle  "build/lib/mathlib4-doc-thms-embeddings.olean" <| fun (data: Array <| (String × String) ×  FloatArray) => pure data.size
 
+unsafe def loadEmbeddingsFullDocsTest : IO Nat := 
+  withUnpickle  "build/lib/mathlib4-prompts-embeddings.olean" <| fun (data: Array <| (String × String × Bool) ×  FloatArray) => pure data.size
 
--- #eval loadEmbeddingsFullTest
+-- #eval loadEmbeddingsFullDocsTest
 
 
 def insertByMemo (l: Array <| α × Float)(cost : α → Float)(sizeBound: Nat)
--- pickle_embeddings.lean
+++ pickle_embeddings.lean
@@ -2,7 +2,7 @@ import LeanCodePrompts.Embeddings
 import Mathlib.Util.Pickle
 
 def main : IO Unit := do
-  let embArr ← readEmbeddingsArray
-  pickle "rawdata/mathlib4-thms-embeddings.olean" embArr
-  let embArrFull ← readEmbeddingsFullArray
-  pickle "rawdata/mathlib4-doc-thms-embeddings.olean" embArrFull
+  let embArrDocs ← readEmbeddingsDocsArray
+  pickle "build/lib/mathlib4-doc-thms-embeddings.olean" embArrDocs
+  let embArrFullDocs ← readEmbeddingsFullDocsArray
+  pickle "build/lib/mathlib4-prompts-embeddings.olean" embArrFullDocs
