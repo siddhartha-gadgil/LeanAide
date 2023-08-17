a1526f17b2eed099191c752cb0535c9cc997c16d
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-08-17 14:25:08 +0530

    refactored using

--- LeanAide/Using.lean
+++ LeanAide/Using.lean
@@ -3,15 +3,19 @@ import Mathlib
 open Lean Meta Elab Term Tactic
 open Mathlib.Prelude.Rename
 
+def usingM (type: Expr) : TacticM Unit := 
+  withMainContext do
+  let goal ← getMainGoal 
+  let target ←  mkArrow type (← getMainTarget)
+  let goal1 ← mkFreshExprMVar type
+  let goal2 ← mkFreshExprMVar target 
+  goal.assign (mkApp goal2 goal1)
+  replaceMainGoal [goal1.mvarId!, goal2.mvarId!]
+
 elab "using" l:term : tactic =>
   withMainContext do
-    let goal ← getMainGoal 
-    let target1 ← elabType l
-    let target2 ←  mkArrow target1 (← getMainTarget)
-    let goal1 ← mkFreshExprMVar target1
-    let goal2 ← mkFreshExprMVar target2 
-    goal.assign (mkApp goal2 goal1)
-    replaceMainGoal [goal1.mvarId!, goal2.mvarId!]
+    let type ← elabType l
+    usingM type
 
 example : 1 ≤ 3 := by
   using 1 ≤ 2
