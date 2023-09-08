e9448055680b1d6f18e8369429d6facb26620f82
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-09-08 09:16:38 +0530

    removed codeaction import

--- LeanAide.lean
+++ LeanAide.lean
@@ -1,6 +1,5 @@
 -- import LeanAide.FirstTacticFinder
 import LeanCodePrompts.Translate
-import LeanCodePrompts.CodeAction
 import LeanAide.TheoremElab
 import LeanAide.PremiseData
 import LeanAide.AesopSearch
