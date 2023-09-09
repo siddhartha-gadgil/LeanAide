df2a0dbe585d2342d49a2452e80adb31ae32d40a
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-09-09 05:54:55 +0530

    generic filepath

--- nearest_embeddings_full.lean
+++ nearest_embeddings_full.lean
@@ -37,7 +37,7 @@ unsafe def show_nearest_full (stdin stdout : IO.FS.Stream)
 
 unsafe def main (args: List String) : IO Unit := do
   logTimed "starting nearest embedding process"
-  let picklePath : System.FilePath := "build/lib/mathlib4-prompts-embeddings.olean"
+  let picklePath : System.FilePath := "build" / "lib" /"mathlib4-prompts-embeddings.olean"
   unless ← picklePath.pathExists do
     IO.eprintln "Fetching embeddings ..."
     let out ← IO.Process.run {
