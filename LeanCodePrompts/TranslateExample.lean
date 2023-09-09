c9267d4006582c4492f38e2223c76ee9dbfa232a
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-09-09 08:08:07 +0530

    version update, tweaks

--- LeanCodePrompts/TranslateExample.lean
+++ LeanCodePrompts/TranslateExample.lean
@@ -6,16 +6,12 @@ import LeanCodePrompts.Translate
 
 -/
 
-#eval logTimed "pinging process"
-#eval pingEmbedding
-#eval logTimed "pinged process"
 -- set_option trace.Translate.info true 
--- #eval translateViewM "There are infinitely many odd numbers"
 #eval translateViewM "If a vector space has dimension `2` then it is finite dimensional."
 
 #eval translateViewM "Every prime number is either `2` or odd"
 
-theorem infinitude_odds : l!"There are infinitely many even numbers" :=
+theorem infinitude_odds : l!"There are infinitely many odd numbers" :=
   by
     sorry 
 
