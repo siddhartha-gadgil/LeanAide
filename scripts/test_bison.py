800d4950c24076d5469816e9905dc66db9ce0dd3
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-10-10 15:25:19 +0530

    more running

--- scripts/test_bison.py
+++ scripts/test_bison.py
@@ -2,7 +2,7 @@ import vertexai
 import json
 from vertexai.preview.language_models import CodeGenerationModel
 
-filename = 'results/gpt4-20prompts-1choice-temp0/silly-results.json'
+filename = 'results/gpt4-20prompts-1choice-temp0/thm-results.json'
 with open(filename, 'r') as f:
     js_lines = json.loads(f.read())
 
@@ -14,20 +14,32 @@ parameters = {
     ],
     "temperature": 0.2
 }
+
+def mkPrompt(js):
+    s ='The following are examples of documentation strings and their corresponding Lean 4 statements.\n\n'
+    for p in json.loads(js['prompts']):
+        s += p['docString'] + '\n'
+        s += p['theorem'] + '\n\n'
+    s +=  'Translate the following documentation string to a Lean 4 statement ONLY.'
+    s += js['docString']
+    return s
 model = CodeGenerationModel.from_pretrained("code-bison@001")
 model = model.get_tuned_model(
     "projects/882229164/locations/us-central1/models/5188329289661022208")
 for js in js_lines:
-    print(js['docString'])
-    response = model.predict(
-        prefix="""Translate to Lean 4 statement ONLY:
-
-        """ + js['prompt_cons'],
-        **parameters
-    )
-    js['bison'] = response.text
-    print(js['bison'])
-    print('---')
+    try:
+        print(js['docString'])
+        response = model.predict(
+            prefix=mkPrompt(js),
+            **parameters
+        )
+        js['bison'] = response.text
+        print(js['bison'])
+        print('---')
+    except Exception as e:
+        print(e)
+        print('---')
+        
 
 with open(filename, 'w') as f:
     f.write(json.dumps(js_lines, indent=4, ensure_ascii=False))
