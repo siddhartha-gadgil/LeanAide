3fc4c1663c5b6297e67cdc2f08d07cf2054a9b1c
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-12-21 12:26:35 +0530

    examples tweaked

--- LeanCodePrompts/TranslateExample.lean
+++ LeanCodePrompts/TranslateExample.lean
@@ -6,13 +6,10 @@ import LeanCodePrompts.Translate
 
 -/
 
--- set_option lean_aide.translate.model "gpt4" -- remove to use "gpt-3.5-turbo"
-#eval logTimed "pinging process"
-#eval pingEmbedding
-#eval logTimed "pinged process"
+set_option lean_aide.translate.model "gpt-4-1106-preview" -- remove to use "gpt-3.5-turbo"
 set_option trace.Translate.info true
--- #eval translateViewM "There are infinitely many odd numbers"
--- #eval translateViewM "If a vector space has dimension `2` then it is finite dimensional." (model := "bert")
+#eval translateViewM "There are infinitely many odd numbers"
+#eval translateViewM "If a vector space has dimension `2` then it is finite dimensional."
 
 #eval translateViewM "Every prime number is either `2` or odd"
 
