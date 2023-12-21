096e2d95a8cc76dc07d6babb2f911d675f777e95
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-12-21 11:53:44 +0530

    tweak with update

--- pickle_embeddings.lean
+++ pickle_embeddings.lean
@@ -2,7 +2,7 @@ import LeanCodePrompts.Embeddings
 import Std.Util.Pickle
 
 def main : IO Unit := do
-  let embArrDocs ← readEmbeddingsDocsArray
-  pickle ".lake/build/lib/mathlib4-doc-thms-embeddings.olean" embArrDocs
+  -- let embArrDocs ← readEmbeddingsDocsArray
+  -- pickle ".lake/build/lib/mathlib4-doc-thms-embeddings.olean" embArrDocs
   let embArrFullDocs ← readEmbeddingsFullDocsArray
   pickle ".lake/build/lib/mathlib4-prompts-embeddings.olean" embArrFullDocs
