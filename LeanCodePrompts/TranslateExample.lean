9d3f4064c163e47b5cd0fa2d9b6538a18e41315e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-12-28 16:35:41 +0530

    example with "condensation points"

--- LeanCodePrompts/TranslateExample.lean
+++ LeanCodePrompts/TranslateExample.lean
@@ -12,6 +12,10 @@ set_option trace.Translate.info true
 #eval translateViewM "There are infinitely many odd numbers"
 #eval translateViewM "If a vector space has dimension `2` then it is finite dimensional."
 
+#eval translateViewM "Suppose $E\\subset\\mathbb{R}^k$ is uncountable, and let $P$ be the set of condensation points of $E$. Then $P$ is perfect."
+
+#eval translateViewM "Suppose $E\\subset\\mathbb{R}^k$ is uncountable, and let $P$ be the set of accumulation points of $E$. Then $P$ is perfect."
+
 #eval translateViewM "Every prime number is either `2` or odd"
 
 -- set_option lean_aide.translate.model "bert"
