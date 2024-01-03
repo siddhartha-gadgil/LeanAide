36f8b59aea23810efe0c6719bc2b21a753359826
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-01-03 17:27:17 +0530

    simple run

--- LeanCodePrompts/Translate.lean
+++ LeanCodePrompts/Translate.lean
@@ -209,6 +209,7 @@ def genericQuery(url: String)(messages: Json)(n: Nat := 1)
   let data := dataJs.pretty
   trace[Translate.info] "Model query: {data}"
   IO.eprintln s!"Querying {url} at {← IO.monoMsNow }"
+  let start ← IO.monoMsNow
   let out ←  IO.Process.output {
         cmd:= "curl",
         args:= #[url++"/v1/chat/completions",
@@ -216,7 +217,7 @@ def genericQuery(url: String)(messages: Json)(n: Nat := 1)
         "-H", "Content-Type: application/json",
         "--data", data]}
   trace[Translate.info] "Model response: {out.stdout} (stderr: {out.stderr})"
-  IO.eprintln s!"Received response from {url} at {← IO.monoMsNow }"
+  IO.eprintln s!"Received response from {url} at {← IO.monoMsNow }; time taken: {(← IO.monoMsNow) - start}"
   return Lean.Json.parse out.stdout |>.toOption.get!
 
 
