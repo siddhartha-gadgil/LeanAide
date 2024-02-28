a5d33da26cfcade0bde341c076b5661b9f3222ad
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-02-28 09:41:08 +0530

    experiments

--- Scratch/Fiddle4.lean
+++ Scratch/Fiddle4.lean
@@ -3,7 +3,6 @@ import Mathlib
 
 open Lean Meta Elab Term PrettyPrinter Tactic
 
-
 @[aesop unsafe 10% tactic]
 def myRing : TacticM Unit := do
   evalTactic (← `(tactic|ring))
@@ -29,3 +28,43 @@ example : ∀ x: ℝ, (x + sin (2 * (cos x))) / (tan x + 1) =
 #check HasDerivAt
 
 #check deriv -- 0 if the function is not differentiable at x.
+
+-- code of Adam Topaz
+def parseFloat (s : String) : Except String Float :=
+  match Lean.Json.parse s with
+    | .ok (.num t) => .ok t.toFloat
+    | _ => throw "Failed to parse as float."
+
+#eval parseFloat "25.123651"
+
+def checkTermType (s: String) (type: Expr) : TermElabM Bool := do
+  let termStx := Parser.runParserCategory (← getEnv) `term s
+  match termStx with
+  | Except.ok termStx =>
+    withoutErrToSorry do
+      try
+        let _ ← elabTermEnsuringType termStx type
+        return true
+      catch err =>
+        logWarning m!"{err.toMessageData}"
+        return false
+  | Except.error err =>
+    logWarning m!"{err}"
+    return false
+
+def checkTermNat (s: String) : TermElabM Bool := do
+  let type :=  Lean.mkConst `Nat
+  checkTermType s type
+
+#eval checkTermNat "3" -- true
+#eval checkTermNat "3 + 4" -- true
+#eval checkTermNat "3 + 4 + 5" -- true
+#eval checkTermNat "3 + 4 + 5 + six" -- false
+#eval checkTermNat "Nat" -- false
+
+def diffNat (n: Nat)(m: Nat := n) : Nat :=
+  n - m
+
+#eval diffNat 4 3
+
+#eval diffNat 4
