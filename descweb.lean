9d1d7a1eeca92937e2c03621a879309963b3c62f
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-04-29 15:46:32 +0530

    reasonable output for descriptions

--- descweb.lean
+++ descweb.lean
@@ -10,11 +10,11 @@ set_option compiler.extract_closed false
 def coreContext : Core.Context := {fileName := "", fileMap := ⟨"", #[], #[]⟩, maxHeartbeats := 100000000000, maxRecDepth := 1000000
     }
 
-def ul (name: Name)(js: Json) : Option String :=
+def block (name: Name)(js: Json) : Option String :=
   let desc? := js.getObjValAs? String "description"
   let statement? := js.getObjValAs? String "statement"
   match desc?, statement? with
-  | Except.ok desc, Except.ok statement => some s!"<li><ul>\n<li><strong>Name:</strong> {name}</li>\n<li>{statement}</li>\n<li>{desc}</li>\n</ul></li>"
+  | Except.ok desc, Except.ok statement => some s!"<tr><td><h4>Name: {name}</h4>\n<h5>{statement}</h5>\n<p>{desc}</p>\n</td></tr>\n"
   | _, _ => none
 
 def main : IO Unit := do
@@ -36,18 +36,40 @@ def main : IO Unit := do
   let mp ← core.run' coreContext {env := env} |>.runToIO'
   IO.println s!"{mp.size} module pairs"
   IO.println s!"{dataMap.size} descriptions"
-  for i in [0:10] do
+  let head ← IO.FS.readFile (System.mkFilePath ["rawdata", "docs", "head.html"])
+  let tail ← IO.FS.readFile (System.mkFilePath ["rawdata", "docs", "tail.html"])
+  IO.FS.writeFile (System.mkFilePath ["rawdata", "docs", "index.html"]) head
+  let indexHandle ← IO.FS.Handle.mk (System.mkFilePath ["rawdata", "docs", "index.html"]) IO.FS.Mode.append
+  indexHandle.putStrLn "<h1>LeanAide data</h1>"
+  indexHandle.putStrLn "<ul class=\"lead\">"
+  for i in [0:50] do
     let (module, consts) := mp.get! i
+    let mut counts := 0
+    let file := System.mkFilePath ["rawdata", "docs", s!"{module}.html"]
+    IO.FS.writeFile file head
+    let h ← IO.FS.Handle.mk file IO.FS.Mode.append
     IO.println s!"Module: {module}"
+    h.putStrLn s!"<h2>Module: {module}</h2>\n<table class=\"table table-striped\">\n<tbody>"
     for n in consts do
       match dataMap.find? n with
       | some js =>
-        match ul n js with
-        | some ul => IO.println ul
+        match block n js with
+        | some ul =>
+          IO.println ul
+          h.putStr ul
+          counts := counts + 1
         | none =>
           -- IO.println s!"skipped (no description): {n}"
           pure ()
       | none =>
           -- IO.println s!"skipped (no data): {n}"
           pure ()
+    h.putStrLn "</tbody>\n</table>"
+    h.putStrLn tail
+    h.flush
+    if counts > 0 then
+      indexHandle.putStrLn s!"<li><a href=\"{module}.html\">{module}</a></li>"
+
     IO.println "-----------------"
+  indexHandle.putStrLn "</ul>"
+  indexHandle.putStrLn tail
