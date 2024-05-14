ec8d764c733ddf68359aef499e9359c2a1cf38f0
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-05-14 10:49:13 +0530

    backport updates

--- nearest_embeddings_full.lean
+++ nearest_embeddings_full.lean
@@ -73,6 +73,7 @@ unsafe def main (args: List String) : IO Unit := do
   let picklePath ← picklePath descField
   withUnpickle  picklePath <|
     fun (data : Array <| (String × String × Bool × String) ×  FloatArray) => do
+        IO.eprintln s!"Searching among {data.size} embeddings"
         let num := (args[1]?.bind fun s => s.toNat?).getD 10
         logTimed s!"finding nearest to `{doc}`"
         let start ← IO.monoMsNow
@@ -90,6 +91,7 @@ unsafe def main (args: List String) : IO Unit := do
         logTimed "found nearest"
         IO.eprintln s!"Time taken: {finish - start} ms"
   | none =>
+    logTimed "No arguments provided, starting interactive mode"
     withUnpickle (← picklePath "docString") <|fun
     (docStringData : Array <| (String × String × Bool × String) ×  FloatArray) =>
     do
