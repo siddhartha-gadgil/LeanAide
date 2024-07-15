47d98009ba40f6c6777d2f017b8c3c70aea8e6d6
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-07-15 10:05:28 +0530

    working with gcloud buckets

--- scripts/gemini_structured.py
+++ scripts/gemini_structured.py
@@ -1,7 +1,7 @@
 import os
 from os.path import join
 from pathlib import Path
-from queries import extract_json_block
+from .queries import extract_json_block
 
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part
@@ -39,6 +39,20 @@ def structured_proof(thm, pf):
 def structured_proof_from_image(thm, path):
     pf = solution_from_image(path)
     return pf, structured_proof(thm, pf)
+
+from google.cloud import storage
+def images_in_gs(prefix):
+    client = storage.Client()
+    blobs = client.list_blobs('leanaide', prefix=prefix)    
+    return [blob.name for blob in blobs if blob.content_type == 'image/png']
+
+def solutions_from_images(thm, prefix):
+    image_paths = images_in_gs(prefix)
+    triples = []
+    for path in image_paths:
+        pf, structured = structured_proof_from_image(thm, path)
+        triples.append((path, pf, structured))
+    return triples
 
 # thm = "The map $p \colon \mathbb{R}^2 \to X$, defined by $p(x,y) = (x, y^2)$, is a covering map, where $X = \{(x,y) \in \mathbb{R}^2 : y \ge 0\}$."
 
