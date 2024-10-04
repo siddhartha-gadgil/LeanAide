6a484981f735d40e428c8de71d87659d4d877dd2
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-10-04 12:05:22 +0530

    adding definitions in translate environment

--- LeanAide/TranslateM.lean
+++ LeanAide/TranslateM.lean
@@ -2,6 +2,8 @@ import Lean
 import LeanAide.Aides
 import Batteries.Util.Pickle
 import LeanAide.SimpleFrontend
+import LeanAide.ConstDeps
+import LeanAide.PremiseData
 
 open Lean Meta Elab Term
 
@@ -15,6 +17,7 @@ structure Translate.State where
   /-- Descriptions, docstrings etc -/
   descriptionMap : HashMap Name Json := HashMap.empty
   cmdPrelude : Array String := #[]
+  defs : Array DefData := #[]
   context : Option String := none
 deriving Inhabited
 
@@ -100,6 +103,13 @@ def runCommand (cmd: String) : TranslateM Unit := do
   discard <|  runFrontendM cmd true
   modify fun s  => {s with cmdPrelude := s.cmdPrelude.push cmd}
 
+def addDefn (dfn: DefData) : TranslateM Unit := do
+  runCommand <| ← dfn.statement
+  modify fun s => {s with defs := s.defs.push dfn}
+
+def defsBlob : TranslateM <| Array String := do
+  let defs := (← get).defs
+  defs.mapM <| (·.statement)
 namespace TranslateM
 
 def runToCore (x: TranslateM α) : CoreM α := do
