630e5e434a5d043931e2046743272c1f45c51760
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2024-11-05 11:11:48 +0530

    pickle script using gcloud storage

--- scripts/pickle.sh
+++ scripts/pickle.sh
@@ -1,4 +1,4 @@
 lake exe pickle_embeddings
 lake exe pickle_embeddings mathlib4-descs-embeddings.json description "description-embedding"
 lake exe pickle_embeddings mathlib4-descs-embeddings.json concise-description "concise-description-embedding"
-rsync -vz .lake/build/lib/mathlib4-*.olean  math.iisc.ac.in:~/public_html/data/
+gcloud storage cp .lake/build/lib/mathlib4-*.olean  gs://leanaid_data/
