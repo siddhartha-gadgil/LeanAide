5c35d67c5fff398cef20587fa5cadabd99cc760e
Malhar Patel <malhar30000@gmail.com>
2024-12-19 16:08:35 +0530

    edited small_embeddings_prompt function in openai_embeddings.py

--- scripts/openai_embeddings.py
+++ scripts/openai_embeddings.py
@@ -29,45 +29,11 @@ def ada_embeddings():
     inp.close()
     out.close()
 
-'''
-def small_embeddings_prompt():
-    inp = open("resources/mathlib4-prompts.json", 'r', encoding='utf-8')
-    out = open("rawdata/mathlib4-docStrings-small-embeddings.json", 'w', encoding='utf-8')
-    client = OpenAI()
-    out_lines = []
-    count = 0
-    """
-    Wrong.
-    Should be like `ada_embeddings` above but
-    with the correct embedding code and modified 
-    output filename.
-    """
-    with open("rawdata/mathlib4-embeddings-docStrings-small.json", 'w', encoding='utf-8') as out:
-        with jsonlines.open("resources/mathlib4-descs.jsonl", 'r') as reader:
-            for l in reader:
-                for field in ["docString", "description", "concise-description"]:
-                    if field in l and l[field]:
-                        response = client.embeddings.create(
-                            input=l[field],
-                            model="text-embedding-3-small"
-                        )
-                        embedding = response.data[0].embedding
-                        l[field + "-embedding"] = embedding
-                        print("Field: ", field)
-                        print(l[field])
-                    else:
-                        print(f"Field {field} not found")
-                out_lines.append(l)
-                count = count + 1
-                print(f"Completed {count}")
-            json.dump(out_lines, out, indent=4, ensure_ascii=False)
-'''
 
 def small_embeddings_prompt():
     client = OpenAI()
     out_lines = []
     count = 0
-
     with open("resources/mathlib4-prompts.json", 'r', encoding='utf-8') as inp, open("rawdata/mathlib4-docStrings-small-embeddings.json", 'w', encoding='utf-8') as out:
         js = json.load(inp)
         for l in js:
@@ -80,16 +46,10 @@ def small_embeddings_prompt():
             l["embedding"] = embedding
             out_lines.append(l)
             count = count + 1
-            
-            '''
-            if count>=5:
-                break
-            '''
-
             print(l["docString"])
             print(f"Completed {count}")
-    
         json.dump(out_lines, out, indent=4, ensure_ascii=False)
+
 
 def small_embeddings_descs():
     client = OpenAI()
