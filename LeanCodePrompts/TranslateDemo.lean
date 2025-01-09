1ec3ac86ef73d2a0d648c8562aa82552940feb05
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-01-09 10:14:27 +0530

    syntax tweak

--- LeanCodePrompts/TranslateDemo.lean
+++ LeanCodePrompts/TranslateDemo.lean
@@ -4,7 +4,7 @@ import LeanAide.Syntax
 
 set_option lean_aide.translate.greedy true
 
-#theorem silly "If a vector space has dimension `2` then it is finite dimensional"
+#theorem silly : "If a vector space has dimension `2` then it is finite dimensional"
 
 
 #theorem "There are infinitely many odd numbers"
