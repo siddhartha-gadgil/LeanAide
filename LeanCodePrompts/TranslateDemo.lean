8027520a998d8e08877a32b4e30a3358256a9bdd
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-01-23 04:14:29 +0000

    removed live examples

--- LeanCodePrompts/TranslateDemo.lean
+++ LeanCodePrompts/TranslateDemo.lean
@@ -11,9 +11,3 @@ set_option lean_aide.translate.greedy true
 #ask "Prove that there are infinitely many even numbers"
 
 #def "A group is said to be sane if every proper normal subgroup is cyclic"
-
-#theorem "The smallest perfect group is $A_5$"
-
-#def "A group is defined to be perfect if its Abelianization is trivial."
-
-#check Abelianization
