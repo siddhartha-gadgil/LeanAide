4bbf84ad1b7cdc872b33e373132efd4214bed8cc
AnirudhG07 <anirudhgupta@iisc.ac.in>
2025-06-19 18:58:59 +0530

    st.session_state -> sts. A relief to hands

--- server/tabs/logs_display.py
+++ server/tabs/logs_display.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import st.session_state as sts
 from logging_utils import log_server, log_buffer_clean
 
 st.markdown("<div id='log_top'></div>", unsafe_allow_html=True)    
@@ -7,18 +8,18 @@ st.title("LeanAide Logs")
 st.subheader("Server Website Stdout/Stderr", help = "Logs are written to LeanAide Server LOGFILE and new logs are updated after SUBMIT REQUEST button is clicked.")
 st.info("You can clear all the logs, reverse the order or change wrapping of the logs from the sidebar.")
 
-if "log_order" not in st.session_state:
-    st.session_state.log_order = True  # Default to reverse order
-if "log_wrap" not in st.session_state:
-    st.session_state.log_wrap = True  # Default to wrap lines
+if "log_order" not in sts:
+    sts.log_order = True  # Default to reverse order
+if "log_wrap" not in sts:
+    sts.log_wrap = True  # Default to wrap lines
 
 # Button here otherwise it will be at the far bottom of the page
 with st.sidebar:
     st.subheader("Server Logs Options", divider = "green")
 
     # Reverse order
-    st.session_state.log_order = st.checkbox("Reverse Order", value=True, help="Check this box to display the logs in reverse order. Default: Display the new logs at the top.")
-    st.session_state.log_wrap = st.checkbox("Wrap Lines", value=True, help="Check this box to wrap the lines in the logs. Default: True")
+    sts.log_order = st.checkbox("Reverse Order", value=True, help="Check this box to display the logs in reverse order. Default: Display the new logs at the top.")
+    sts.log_wrap = st.checkbox("Wrap Lines", value=True, help="Check this box to wrap the lines in the logs. Default: True")
     
     st.write("")
     # Clean logs
@@ -26,7 +27,7 @@ with st.sidebar:
         st.write("Are you sure you want to clean the server logs? This will delete all the logs in the server.")
         if st.button("Yes"):
             try:
-                st.session_state.log_server_cleaned = True
+                sts.log_server_cleaned = True
                 log_buffer_clean(log_file=True)
                 st.success("Server logs cleaned successfully! Please UNCHECK THE BOX to avoid cleaning again.")
                 st.rerun()
@@ -34,19 +35,19 @@ with st.sidebar:
                 st.error(f"Error cleaning server logs: {e}")
         if st.button("No"):
             pass
-        st.session_state.log_server_cleaned = False
+        sts.log_server_cleaned = False
         st.info("Press Escape to close this popover.")
 
         
-if log_out := log_server(log_file=True, order = st.session_state.log_order):
-    if st.session_state.log_order:
+if log_out := log_server(log_file=True, order = sts.log_order):
+    if sts.log_order:
         st.write("Logs are displayed in newest first order.")
     else:
         st.write("Logs are displayed in oldest first order.")
     st.code(
-        log_out if not st.session_state.log_server_cleaned else "No logs available yet.",
+        log_out if not sts.log_server_cleaned else "No logs available yet.",
         language = "log",
-        wrap_lines=st.session_state.log_wrap,
+        wrap_lines=sts.log_wrap,
         line_numbers=True,
     )
 else:
--- server/tabs/server_response.py
+++ server/tabs/server_response.py
@@ -4,6 +4,7 @@ import json
 
 import requests
 import streamlit as st
+import st.session_state as sts
 from dotenv import load_dotenv
 
 from api_server import HOST, PORT
@@ -18,15 +19,15 @@ st.write(
 )
 st.info("Ensure the Host information of the backend server to query is correct. Check out the sidebar for host information settings.")
 
-if "api_host" not in st.session_state:
-    st.session_state.api_host = HOST
-if "api_port" not in st.session_state:
-    st.session_state.api_port = PORT
-if "task_tbd" not in st.session_state:
-    st.session_state.task_tbd = []
+if "api_host" not in sts:
+    sts.api_host = HOST
+if "api_port" not in sts:
+    sts.api_port = PORT
+if "task_tbd" not in sts:
+    sts.task_tbd = []
 
-if not st.session_state.val_input:
-    st.session_state.val_input = {}
+if not sts.val_input:
+    sts.val_input = {}
 
 # Host Information Section
 with st.sidebar:
@@ -44,13 +45,13 @@ with st.sidebar:
                 value= HOST if HOST not in ["localhost", "127.0.0.1"] else local_ip,
                 help="Specify the hostname or IP address where the proof server is running. Default is localhost.",
             )
-            st.session_state.api_host = api_host
+            sts.api_host = api_host
         api_port = st.text_input(
             "API Port:",
             value="7654",
             help="Specify the port number where the proof server is running. Default is 7654.",
         )
-        st.session_state.api_port = api_port
+        sts.api_port = api_port
 
 st.header("Server Request", divider = True, help = "For your input request, this request will be sent to the backend server specified by you.")
 
@@ -64,15 +65,15 @@ with st.expander("Load input Conversation from JSON"):
     if uploaded_file:
         try:
             json_data = json.load(uploaded_file)
-            st.session_state.val_input = json_data["input"]
-            st.session_state.task_tbd = json_data["tasks"]
-            st.session_state.temp_structured_json = json_data["input"]["json_structured"] if "json_structured" in json_data["input"] else {}
-            st.session_state.self_input_button = True
-            st.session_state.valid_input = True
+            sts.val_input = json_data["input"]
+            sts.task_tbd = json_data["tasks"]
+            sts.temp_structured_json = json_data["input"]["json_structured"] if "json_structured" in json_data["input"] else {}
+            sts.self_input_button = True
+            sts.valid_input = True
             try:
-                st.session_state.result = json_data["output"]
-                st.session_state.request_button = True
-                st.session_state.server_output_success = True
+                sts.result = json_data["output"]
+                sts.request_button = True
+                sts.server_output_success = True
             except Exception as e:
                 log_write("Streamlit", f"Error loading 'output' from JSON: {e}. Skipping")
                 st.warning("No 'output' found in the JSON file(Skipping). You can still fill inputs and generate new output.")
@@ -93,10 +94,10 @@ with st.expander("Load input Conversation from JSON"):
 
 st.subheader("Structured Input: Select Tasks", help = "Select the tasks you want to perform and provide the necessary inputs.")
 
-st.session_state._task_tbd = st.session_state.task_tbd
+sts._task_tbd = sts.task_tbd
 # list of tasks, each task has "name" field. use that
-st.multiselect("Select task(s) to be performed:", list(reversed(list(TASKS.keys()))), help = "Select the tasks to be performed by the backend server. You can select multiple tasks.", key = "_task_tbd", on_change=lambda: setattr(st.session_state, "task_tbd", st.session_state._task_tbd))
-st.session_state.selected_tasks = st.session_state.task_tbd
+st.multiselect("Select task(s) to be performed:", list(reversed(list(TASKS.keys()))), help = "Select the tasks to be performed by the backend server. You can select multiple tasks.", key = "_task_tbd", on_change=lambda: setattr(sts, "task_tbd", sts._task_tbd))
+sts.selected_tasks = sts.task_tbd
 
 ## Multiselect box color set
 st.markdown("""
@@ -111,9 +112,9 @@ span[data-baseweb="tag"] {
 
 """, unsafe_allow_html=True)
 
-if st.button("Build Query", help = "Provide inputs to the your selected tasks. Note: It is not compulsary to fill all of them.", type = "primary") or st.session_state.self_input_button:
-    st.session_state.self_input_button = True
-    for task in st.session_state.selected_tasks:
+if st.button("Build Query", help = "Provide inputs to the your selected tasks. Note: It is not compulsary to fill all of them.", type = "primary") or sts.self_input_button:
+    sts.self_input_button = True
+    for task in sts.selected_tasks:
         # Get input for each task
         for key, val_type in TASKS[task].get("input",{}).items():
             help = f"Please provide input for `{key}` of type `{val_type}`."
@@ -121,80 +122,80 @@ if st.button("Build Query", help = "Provide inputs to the your selected tasks. N
             if key.lower() == "json_structured":
                 help += " Just paste your `json` object here."
                 if st.button("Use Structured JSON generated", help = "Use the structured JSON generated in the `Structured Json` page of LeanAide website.", key = f"use_structured_json_{task}"):
-                    if structured_json := st.session_state.get("structured_proof", {}):
-                        st.session_state.temp_structured_json = structured_json 
+                    if structured_json := sts.get("structured_proof", {}):
+                        sts.temp_structured_json = structured_json 
                     else:
                         st.warning("No structured JSON found. Please generate it first in the 'Structured Json' page.")
  
-                val_in = st.text_area(f"{task.capitalize()} - {key} ({val_type}):", help = help, placeholder = "{'key': 'value'}", value = st.session_state.temp_structured_json)
-                st.session_state.temp_structured_json = val_in  # Store the structured JSON input
+                val_in = st.text_area(f"{task.capitalize()} - {key} ({val_type}):", help = help, placeholder = "{'key': 'value'}", value = sts.temp_structured_json)
+                sts.temp_structured_json = val_in  # Store the structured JSON input
 
             # Other cases for input
             elif "json" in key.lower() and key.lower() != "json_structured":
                 help += " Just paste your `json` object here."
-                val_in = st.text_area(f"{task.capitalize()} - {key} ({val_type}):", help = help, placeholder = "{'key': 'value', etc}", value = st.session_state.val_input.get(key, ""))
+                val_in = st.text_area(f"{task.capitalize()} - {key} ({val_type}):", help = help, placeholder = "{'key': 'value', etc}", value = sts.val_input.get(key, ""))
             else:
-                val_in = st.text_area(f"{task.capitalize()} - {key} ({val_type}):", help = help, value = st.session_state.val_input.get(key, ""))
+                val_in = st.text_area(f"{task.capitalize()} - {key} ({val_type}):", help = help, value = sts.val_input.get(key, ""))
 
             if str(val_in).strip() == "":
-                st.session_state.val_input[key] = None
+                sts.val_input[key] = None
             ## Put input value in session state
             if val_in:
-                inp_type, st.session_state.val_input[key] = get_actual_input(val_in)
+                inp_type, sts.val_input[key] = get_actual_input(val_in)
                 if validate_input_type(inp_type, val_type):
-                    st.session_state.valid_input = True
+                    sts.valid_input = True
                 else:
                     st.warning(
                         f"Invalid input type for {key}. Expected `{val_type}`, got `{inp_type.__name__}`. See help for each input for more info. Please try again."
                     )
-                    st.session_state.valid_input = False
-                    st.session_state.val_input[key] = None
+                    sts.valid_input = False
+                    sts.val_input[key] = None
 
         # Parameters for each task
         for param, param_type in TASKS[task].get("parameters", {}).items():
             if "boolean" in param_type.lower() or "bool" in param_type.lower():
-                st.session_state.valid_input = True
+                sts.valid_input = True
 
                 # Checked for True, Unchecked for False
                 default_val = True if "true" in param_type.lower() else False
-                st.session_state.val_input[param] = default_val
+                sts.val_input[param] = default_val
                 checkbox_state = st.checkbox(
                     f"{task.capitalize()} - {param} (Boolean)",
                     help="Checked box: `True`, Unchecked box: `False`",
                     value=default_val
                 )
-                st.session_state.val_input[param] = checkbox_state
+                sts.val_input[param] = checkbox_state
             else:
                 help = f"Please provide a value for `{param}` of type `{param_type}`. If you want to skip this parameter, just leave it unchecked."
                 if param_in := st.text_input(f"{task.capitalize()} - {param} ({param_type}):", help=help):
-                    st.session_state.val_input[param] = param_in
-                    st.session_state.valid_input = True
+                    sts.val_input[param] = param_in
+                    sts.valid_input = True
 
-    if st.session_state.valid_input:
+    if sts.valid_input:
         st.subheader("Query Obtained", help = "Note that default values will be used for any parameters that you did not provide input for.")
         # Remove the values in val_input that are empty strings or None
-        st.session_state.val_input = {k: v for k, v in st.session_state.val_input.items() if v not in ["", None]}
-        st.json(st.session_state.val_input)
+        sts.val_input = {k: v for k, v in sts.val_input.items() if v not in ["", None]}
+        st.json(sts.val_input)
         log_write("Streamlit", "Query Obtained: Success")
 
 st.write("")
 # Show Response function
 def show_response():
-    for task in st.session_state.selected_tasks:
+    for task in sts.selected_tasks:
         st.subheader(task + " Output", divider =True)
         for key, val_type in TASKS[task]["output"].items():
             if "json" in val_type.lower().split():
                 st.write(f"{key.capitalize()} ({val_type}):")
-                json_out = st.session_state.result.get(key) or {}
+                json_out = sts.result.get(key) or {}
                 st.json(json_out)
                 copy_to_clipboard(str(json_out))
             else:
                 st.write(f"{key.capitalize()} ({val_type}):")
                 st.code(
-                    st.session_state.result.get(key) or "No data available.", language="plaintext", wrap_lines = True
+                    sts.result.get(key) or "No data available.", language="plaintext", wrap_lines = True
                 )
                 if "lean_code" in key.lower():
-                    code = st.session_state.result.get(key, "-- No Lean code available")
+                    code = sts.result.get(key, "-- No Lean code available")
                     code = f"import Mathlib\n{code.strip()}" if "import Mathlib" not in code else code
                     if code not in ["-- No Lean code available", "No data available."]:
                         st.link_button("Open Lean Web IDE", help="Open the Lean code in the Lean Web IDE.", url = f"https://live.lean-lang.org/#code={urllib.parse.quote(code)}")
@@ -212,7 +213,7 @@ def dummy_request():
         "data": "Dummy Request."
     }
     response = requests.post(
-        f"http://{st.session_state.api_host}:{st.session_state.api_port}", json=request_payload
+        f"http://{sts.api_host}:{sts.api_port}", json=request_payload
     )
 
     if response.status_code == 200:
@@ -224,78 +225,78 @@ def dummy_request():
 
 # Submit Request Section. 
 submit_response_button =  st.button("Submit Request", on_click= button_clicked("request_button"), type = "primary", help = "You can submit your request here. The request will be sent to the backend server specified in the Host Information section.")
-if submit_response_button or st.session_state.request_button:
+if submit_response_button or sts.request_button:
     dummy_request() # Just a dummy request
     request_payload = {}
-    if not st.session_state.selected_tasks:
+    if not sts.selected_tasks:
         st.warning("Please Input tasks to be performed.")
         log_write("Streamlit", "Request Payload: Invalid Input")
-    elif not st.session_state.valid_input:
+    elif not sts.valid_input:
         st.warning("Please provide valid inputs for the selected tasks.")
         log_write("Streamlit", "Request Payload: Invalid Input")
     else:
-        server_tasks = [TASKS[task]["task_name"] for task in st.session_state.selected_tasks]
-        request_payload = {"tasks": server_tasks, **st.session_state.val_input}
+        server_tasks = [TASKS[task]["task_name"] for task in sts.selected_tasks]
+        request_payload = {"tasks": server_tasks, **sts.val_input}
 
         if submit_response_button:
             with st.spinner("Request sent. Please wait for a short while..."): 
                 log_write("Streamlit", f"Request Payload: {request_payload}")
                 response = requests.post(
-                    f"http://{st.session_state.api_host}:{st.session_state.api_port}", json=request_payload
+                    f"http://{sts.api_host}:{sts.api_port}", json=request_payload
                 )
 
             if response.status_code == 200:
                 # Get the result
                 st.success("Response sent and received successfully!")
-                st.session_state.result = response.json()
-                log_write("Streamlit", f"Selected Tasks: {st.session_state.selected_tasks}")
-                log_write("Streamlit", f"Response: {st.session_state.result}")
+                sts.result = response.json()
+                log_write("Streamlit", f"Selected Tasks: {sts.selected_tasks}")
+                log_write("Streamlit", f"Response: {sts.result}")
 
                 try:
-                    if st.session_state.result["result"] == "success":
-                        st.session_state.server_output_success = True
+                    if sts.result["result"] == "success":
+                        sts.server_output_success = True
                         st.success("Request processed successfully!")
                     else: # result = "error"
-                        st.session_state.server_output_success = False
+                        sts.server_output_success = False
                         st.error("Error in processing the request. Please check the input and try again.")
                         st.write("Error (String):")
-                        st.code(st.session_state.result["error"])
+                        st.code(sts.result["error"])
                     log_write("Streamlit", "Server Output: Success")
                 except Exception as e:
-                    st.session_state.server_output_success = False
+                    sts.server_output_success = False
                     st.error(f"Error in processing the request: {e}")
                     st.write("Response (String):")
-                    st.code(str(st.session_state.result))
+                    st.code(str(sts.result))
                     log_write("Streamlit", f"Server Output: Error - {e}")
 
             else:
-                st.session_state.server_output_success = False
+                sts.server_output_success = False
                 st.error(f"Error: {response.status_code}, {response.text}")
                 log_write("Streamlit", f"Server Response Error: {response.status_code} - {response.text}")
                 # Handle the output for each task
-        if st.session_state.server_output_success:
+        if sts.server_output_success:
             show_response()
         else:
             st.error("No output available. Please check the input and try again.")
             try:
-                st.error(st.session_state.result.get("error", "No error message provided."))
+                st.error(sts.result.get("error", "No error message provided."))
             except Exception as e:
                 st.error(f"Error retrieving error from server: {e}")
                 pass
             log_write("Streamlit", "Server Output: No output available.")
 
 
-elif st.session_state.request_button:
+elif sts.request_button:
     show_response()
 
 st.divider()
 
 if st.checkbox("`Download Conversation:` Save your input and output response in a JSON file and download it."):
-    if st.session_state.selected_tasks and st.session_state.valid_input:
+    if sts.selected_tasks and sts.valid_input:
         conversation_data = {
-            "tasks": st.session_state.selected_tasks,
-            "input": st.session_state.val_input,
-            "output": st.session_state.result if hasattr(st.session_state, 'result') else {}
+            "tasks": sts.selected_tasks,
+            "input": sts.val_input,
+            "output": sts.result if hasattr(sts, 'result') else {}
         }
         st.download_button(
             label="Download Conversation",
