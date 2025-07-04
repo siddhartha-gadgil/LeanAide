a94a10e7a0fbffd0ecf67109801f7a183b2890ab
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-07-04 08:15:34 +0000

    generalized retranslate to handle elaberrors

--- LeanAide/ReTranslate.lean
+++ LeanAide/ReTranslate.lean
@@ -7,7 +7,7 @@ open Translate Lean Meta Elab Term Qq
 
 /-- Environment extension storing retranslation patterns -/
 initialize retranslateExt :
-    SimpleScopedEnvExtension Name (Array Name) ←
+    SimpleScopedEnvExtension (Name × Bool) (Array (Name × Bool)) ←
   registerSimpleScopedEnvExtension {
     addEntry := fun m n =>
         m.push n
@@ -21,24 +21,42 @@ initialize registerBuiltinAttribute {
   descr := "Prompt for trying to retranslate a theorem or definition."
   add := fun decl stx kind => MetaM.run' do
     let declTy := (← getConstInfo decl).type
-    let expectedType : Q(Type) :=
+    let expectedSimpleType : Q(Type) :=
       q(String → Option String) -- type of the prompt function
-    unless ← isDefEq declTy expectedType do
-      throwError -- replace with error
-        s!"codegen: {decl} has type {declTy}, but expected {expectedType}"
-    retranslateExt.add decl
+    let isSimple ← isDefEq declTy expectedSimpleType
+    let expectedType : Q(Type) :=
+      q(ElabError → Option String) -- type of the prompt function
+    if !isSimple then
+      unless ← isDefEq declTy expectedType do
+        throwError
+          s!"codegen: {decl} has type {declTy}, but expected {expectedSimpleType} or {expectedType}"
+    retranslateExt.add (decl, isSimple)
 }
 
 #check Term.mkConst
 
-def retranslatePrompts : MetaM (Array (String → Option String)) := do
+def retranslateSimplePrompts : MetaM (Array (String → Option String)) := do
   let names := retranslateExt.getState (← getEnv)
-  names.mapM fun n => do
-    let f := Lean.mkConst n
-    unsafe evalExpr (String → Option String) q(String → Option String) f
+  names.filterMapM fun (n, isSimple) => do
+    if isSimple then
+      let f := Lean.mkConst n
+      unsafe evalExpr (String → Option String) q(String → Option String) f
+    else
+      pure none
+
+def retranslatePrompts : MetaM (Array (ElabError → Option String)) := do
+  let names := retranslateExt.getState (← getEnv)
+  names.mapM fun (n, isSimple) => do
+    if isSimple then
+      let f := Lean.mkConst n
+      let f ← unsafe evalExpr (String → Option String) q(String → Option String) f
+      pure <| fun e => f e.text
+    else
+      let f := Lean.mkConst n
+      unsafe evalExpr (ElabError → Option String) q(ElabError → Option String) f
 
 def allRetranslatePrompts (s: String) : MetaM (Array String) := do
-  let prompts ← retranslatePrompts
+  let prompts ← retranslateSimplePrompts
   return prompts.filterMap fun prompt? => prompt? s
 
 def retranslateFromStringsAux (ss: Array String) (prompts: Array (String → Option String)) (translator: Translator := {}) (n: Nat := 3) :
@@ -57,9 +75,26 @@ def retranslateFromStringsAux (ss: Array String) (prompts: Array (String → Opt
         continue
   return .error errs.flatten
 
+def retranslateFromErrorsAux (ss: Array ElabError) (prompts: Array (ElabError → Option String)) (translator: Translator := {}) (n: Nat := 3) :
+    TranslateM <| Except (Array ElabError) Expr := do
+  let mut errs := #[]
+  for prompt in prompts do
+    for s in ss do
+      if let some fixPrompt := prompt s then
+        let results ← translator.server.mathCompletions fixPrompt n
+        match ← greedyBestExprWithErr? results with
+        | Except.ok e =>
+          return Except.ok e
+        | Except.error err =>
+          errs := errs.push err
+      else
+        continue
+  return .error errs.flatten
+
+
 def retranslateFromStrings (ss: Array String) (translator: Translator := {}) (n: Nat := 3) :
     TranslateM <| Except (Array ElabError) Expr := do
-  let prompts ← retranslatePrompts
+  let prompts ← retranslateSimplePrompts
   retranslateFromStringsAux ss prompts translator n
 
 /--
@@ -67,7 +102,7 @@ Attempt to fix the given errors by using the prompt function to generate a promp
 -/
 def retranslateFromErrors (errs: Array ElabError)  (translator: Translator := {}) (n: Nat := 3) :
     TranslateM <| Except (Array ElabError) Expr := do
-  retranslateFromStrings (errs.map (fun e => e.text)) translator n
+  retranslateFromErrorsAux errs (← retranslatePrompts) translator n
 
 
 end LeanAide
--- LeanAideTest/TranslateFix.lean
+++ LeanAideTest/TranslateFix.lean
@@ -6,8 +6,19 @@ open LeanAide Lean Meta Translate
 
 def exCode := "∃ (G : Type) [inst : Group G], ∀ g : G, g * g = 1"
 
+-- @[retranslate]
+-- def existsFixPrompt? (exCode: String) : Option String  :=
+--   if exCode.contains '∃' && exCode.contains '[' then
+--     some <|
+--   s!"The following is **incorrect** Lean Prover code. The probable fix is: For an 'exists' statement, typeclasses should also use parenthesis not square brackets:
+-- {exCode}
+-- Fix the code and output ONLY the corrected code (NOT in a code block and with NO comments)."
+--   else
+--     none
+
 @[retranslate]
-def existsFixPrompt? (exCode: String) : Option String  :=
+def existsFixPrompt? (err: ElabError) : Option String  :=
+  let exCode := err.text
   if exCode.contains '∃' && exCode.contains '[' then
     some <|
   s!"The following is **incorrect** Lean Prover code. The probable fix is: For an 'exists' statement, typeclasses should also use parenthesis not square brackets:
@@ -16,10 +27,11 @@ Fix the code and output ONLY the corrected code (NOT in a code block and with NO
   else
     none
 
+
 def server := ({} : Translator).server
 
-#eval server.mathCompletions <| (existsFixPrompt? exCode).get!
+-- #eval server.mathCompletions <| (existsFixPrompt? exCode).get!
 
-#eval retranslateFromStrings #[exCode]  ({} : Translator) 3
+#eval retranslateFromErrors #[.unparsed exCode "<dummy>" none]  ({} : Translator) 3
 
 #eval allRetranslatePrompts "∃ (G : Type) [inst : Group G], ∀ g : G, g * g = 1"
