3942a6cce1e229de349baab665cd4f0ef3c99016
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-07-14 06:18:11 +0000

    closer to original

--- LeanAideTest/FVarError.lean
+++ LeanAideTest/FVarError.lean
@@ -20,8 +20,9 @@ def eg := json% {
                     "assumption": "[inst : Group G]"
                 },
                 {
-                    "type": "assume_statement",
-                    "assumption": "∃ (g₀ : G), ∀ x : G, ∃ k : ℤ, x = g₀^k"
+                    "type": "some_statement",
+                    "variable_name": "hcyc",
+                    "statement": "∃ (g₀ : G), ∀ x : G, ∃ k : ℤ, x = g₀^k"
                 }
             ],
             "claim": "Every cyclic group is Abelian."
@@ -36,8 +37,57 @@ def eg := json% {
 
 universe u_1
 
-
--- #guard_msgs in
+/--
+info: codegen: trying LeanAide.documentCode for key document
+---
+info: codegen: trying LeanAide.theoremCode for key theorem
+---
+info: codegen: trying LeanAide.assumeCode for key assume_statement
+---
+info: codegen: trying LeanAide.assumeCode for key assume_statement
+---
+info: codegen: trying LeanAide.someCode for key some_statement
+---
+info: codegen: trying LeanAide.assertionCode for key assert_statement
+---
+info: codegen: trying LeanAide.assumeCode for key assume_statement
+---
+info: codegen: trying LeanAide.assumeCode for key assume_statement
+---
+info: codegen: trying LeanAide.someCode for key some_statement
+---
+info: codegen: trying LeanAide.assertionCode for key assert_statement
+---
+info: All theorems : [thm:cyclic_group_abelian]
+---
+info: codegen: trying LeanAide.proofCode for key proof
+---
+info: codegen: trying LeanAide.assumeCode for key assume_statement
+---
+info: codegen: trying LeanAide.assumeCode for key assume_statement
+---
+info: codegen: trying LeanAide.someCode for key some_statement
+---
+info: codegen: trying LeanAide.assertionCode for key assert_statement
+---
+info: Try this: ⏎
+  def cyclic_group_is_abelian.prop : Prop :=
+    ∀ {G : Type u_1} [inst : Group G], (∃ (g₀ : G), ∀ (x : G), ∃ (k : ℤ), x = g₀ ^ k) → ∀ (a b : G), a * b = b * a
+  def deferred.cyclic_group_is_abelian [assume_cyclic_group_is_abelian : Fact cyclic_group_is_abelian.prop] :
+      cyclic_group_is_abelian.prop :=
+    assume_cyclic_group_is_abelian.elim
+  theorem cyclic_group_is_abelian :
+      ∀ {G : Type u_1} [inst : Group G], (∃ (g₀ : G), ∀ (x : G), ∃ (k : ℤ), x = g₀ ^ k) → ∀ (a b : G), a * b = b * a :=
+    by
+    intro G inst_14157295161945824867 a_1008755775771949303 a b
+    trace
+      "Automation Tactics first\n  | (simp?; done)\n  | hammer {aesopPremises := 5, autoPremises := 0} for goal: a * b = b * a"
+    obtain ⟨w, h⟩ := a_1008755775771949303
+    sorry
+    trace
+      "Finished Automation Tactics first\n  | (simp?; done)\n  | hammer {aesopPremises := 5, autoPremises := 0} for goal: a * b = b * a"
+-/
+#guard_msgs in
 #codegen eg
 
 example : ∀ {G : Type u_1} [inst : Group G], ∃ (g₀ : G), ∀ (x : G), ∃ (k : ℤ), x = g₀ ^ k := by
@@ -47,5 +97,5 @@ example : ∀ {G : Type u_1} [inst : Group G], ∃ (g₀ : G), ∀ (x : G), ∃ 
   intro G inst
   sorry
 
--- #eval Translator.translateToPropStrict "Every cyclic group is Abelian." {}
+-- #eval Translator.translateToPropStrict "Every cyclic group is Abelian."
 end LeanAideTest.FVarError
