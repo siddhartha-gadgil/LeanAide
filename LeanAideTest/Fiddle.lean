e145e5e3fcab630cf4e9059e5000d23cea58e4f1
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-07-15 06:05:54 +0000

    fixed regression due to turning of auto-implicits

--- LeanAideTest/Fiddle.lean
+++ LeanAideTest/Fiddle.lean
@@ -1,5 +1,7 @@
 import Mathlib
 import LeanAide
+open Nat LeanAide
+set_option autoImplicit false
 
 
 abbrev hard.prop : Prop := False
@@ -19,3 +21,9 @@ end
 #check hard_copy -- hard_copy [inst_hard : Fact hard.prop] : hard.prop
 
 example : 1 ≤ 2 := by first | (simp? ; done) | hammer
+
+
+universe u
+example : ∀ {G : Type u} [inst : Group G] (a : G) (n : ℕ), a ^ n = 1 → ∃ m : ℕ, n = m * orderOf a := by sorry
+
+#eval elabFrontTheoremExprM "∀ {G : Type u} [inst : Group G] (a : G) (n : ℕ), a ^ n = 1 → ∃ m : ℕ, n = m * orderOf a"
