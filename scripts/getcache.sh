d48fbe643bb1ca0fa8aa12b0ad86de1d1b58a95d
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-08-26 11:16:35 +0000

    unauthenticated cache fetching

--- scripts/getcache.sh
+++ scripts/getcache.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-gcloud storage rsync   gs://leanaide_data/cache/prompt .leanaide_cache/prompt  --recursive
-gcloud storage rsync   gs://leanaide_data/cache/prompt .leanaide_cache/prompt  --recursive
+gcloud storage rsync   gs://leanaide_data/cache/prompt .leanaide_cache/prompt  --recursive || gsutil -m cp -r gs://leanaide_data/cache/prompt .leanaide_cache/prompt
+gcloud storage rsync   gs://leanaide_data/cache/prompt .leanaide_cache/prompt  --recursive || gsutil -m cp -r gs://leanaide_data/cache/prompt .leanaide_cache/prompt
