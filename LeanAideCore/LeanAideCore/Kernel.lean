52248f457a80d27e92b6e68e196b3731079b638e
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-09-01 14:49:22 +0000

    renaming, ideas

--- LeanAideCore/LeanAideCore/Kernel.lean
+++ LeanAideCore/LeanAideCore/Kernel.lean
@@ -10,10 +10,19 @@ namespace LeanAide
 
 open Translate
 
+/-!
+We want the same functions and the same syntax to work in LeanAide itself and a client. To do this, we will have:
+
+* A typeclass `Kernel` capturing these.
+* Given a connection to a LeanAide server (maybe another typeclass), an instance of `Kernel`.
+* Given an instance of `Kernel`, `response` functions that expose the functions over a server.
+-/
+
+
 /--
 The core functions of LeanAide, implemented directly in the server and via querying in the client.
 -/
-class Interface where
+class Kernel where
   translateThm? : Translator → String → TranslateM (Except (Array ElabError) Expr)
   translateDef? : Translator → String → TranslateM (Except (Array CmdElabError) Syntax.Command)
   theoremDoc? : Translator → Syntax.Command → TranslateM (Except String String)
