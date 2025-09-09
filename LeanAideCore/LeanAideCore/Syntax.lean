9028170242dfc67bb00b4418467fac58271d2d6b
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-09-09 12:36:13 +0000

    completed syntax move to core

--- LeanAideCore/LeanAideCore/Syntax.lean
+++ LeanAideCore/LeanAideCore/Syntax.lean
@@ -219,6 +219,47 @@ def getProofStringText [Monad m] [MonadError m] (stx : TSyntax ``proofComment) :
     else
       logWarning "To make a query, end the string with a `.` or `?`."
 
+open Command in
+@[command_elab addDocs] def elabAddDocsImpl : CommandElab := fun stx =>
+  match stx with
+  | `(#doc theorem $id:ident $ty:declSig $val:declVal) =>
+    Command.liftTermElabM do
+    let name := id.getId
+    let stx' ← `(command| theorem $id:ident $ty $val)
+    -- let fmt ← PrettyPrinter.ppCommand stx'
+    -- let type : Expr ← elabFrontThmExprM fmt.pretty name true
+    -- let some (desc, _) ←
+    --   Translator.getTypeDescriptionM type {} | throwError "No description found for type {type}"
+    let desc ← KernelM.theoremDoc name stx'
+    let docs := mkNode ``Lean.Parser.Command.docComment #[mkAtom "/--", mkAtom (desc ++ " -/")]
+    let stx' ← `(command| $docs:docComment theorem $id:ident $ty $val)
+    TryThis.addSuggestion stx stx'
+  | `(#doc def $id:ident $args* : $ty:term := $val:term) =>
+    Command.liftTermElabM do
+    let name := id.getId
+    let stx' ← `(command| def $id:ident $args* : $ty:term := $val:term)
+    -- let fmt ← PrettyPrinter.ppCommand stx'
+    -- let (type, value) ← elabFrontDefTypeValExprM fmt.pretty name true
+    -- let some (desc, _) ←
+    --   Translator.getDefDescriptionM type value name {} | throwError "No description found for type {type}"
+    let desc ← KernelM.defDoc name stx'
+    let docs := mkNode ``Lean.Parser.Command.docComment #[mkAtom "/--", mkAtom (desc ++ " -/")]
+    let stx' ← `(command| $docs:docComment def $id:ident $args* : $ty:term := $val:term)
+    TryThis.addSuggestion stx stx'
+  | `(#doc noncomputable def $id:ident $args* : $ty:term := $val:term) =>
+    Command.liftTermElabM do
+    let name := id.getId
+    let stx' ← `(command| noncomputable def $id:ident $args* : $ty:term := $val:term)
+    -- let fmt ← PrettyPrinter.ppCommand stx'
+    -- let (type, value) ← elabFrontDefTypeValExprM fmt.pretty name true
+    -- let some (desc, _) ←
+    --   Translator.getDefDescriptionM type value name {} | throwError "No description found for type {type}"
+    let desc ← KernelM.defDoc name stx'
+    let docs := mkNode ``Lean.Parser.Command.docComment #[mkAtom "/--", mkAtom (desc ++ " -/")]
+    let stx' ← `(command| $docs:docComment noncomputable def $id:ident $args* : $ty:term := $val:term)
+    TryThis.addSuggestion stx stx'
+  | _ => throwError "unexpected syntax"
+
 
 
 end LeanAide.Meta
