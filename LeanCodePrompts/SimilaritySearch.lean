2ded3f4a371de614320456400f4cfa927f2dca67
Malhar Patel <malhar30000@gmail.com>
2025-09-30 09:19:32 +0000

    Add descField argument to callSimilaritySearch

--- LeanCodePrompts/SimilaritySearch.lean
+++ LeanCodePrompts/SimilaritySearch.lean
@@ -1,6 +1,6 @@
-def callSimilaritySearch (query : String) (numSim : Nat) : IO String := do
+def callSimilaritySearch (descField : String) (query : String) (numSim : Nat) : IO String := do
   let exePath := System.mkFilePath [".", "SimilaritySearch", "SimilaritySearch.py"]
-  let inp ← IO.Process.output {cmd := "python3", args := #[exePath.toString, query, (toString numSim)]}
+  let inp ← IO.Process.output {cmd := "python3", args := #[exePath.toString, query, (toString numSim), descField]}
   let ⟨err_code,stdout,stderr⟩ := inp
   match err_code with
   | 0 => return stdout
