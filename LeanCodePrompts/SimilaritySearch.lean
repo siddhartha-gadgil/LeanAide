8c1373714900a8068e3625950a0530e34daada9b
Malhar Patel <malhar30000@gmail.com>
2025-09-30 08:48:07 +0000

    Match on error code to return stdout or stderr

--- LeanCodePrompts/SimilaritySearch.lean
+++ LeanCodePrompts/SimilaritySearch.lean
@@ -1,9 +1,7 @@
 def callSimilaritySearch : IO String := do
   let exePath := System.mkFilePath [".", "SimilaritySearch", "SimilaritySearch.py"]
-  let inp ← IO.Process.output {cmd := "python3", args := #[exePath.toString, "infinite primes", "10"]}
+  let inp ← IO.Process.output {cmd := "python3", args := #[exePath.toString, "infinite primes", "3"]}
   let ⟨err_code,stdout,stderr⟩ := inp
   match err_code with
-  | 0 => return s!"SUCCESS: {stdout}"
-  | _ => return s!"ERROR THROWN: {stderr}"
-
--- #eval callSimilaritySearch
+  | 0 => return stdout
+  | _ => return stderr
