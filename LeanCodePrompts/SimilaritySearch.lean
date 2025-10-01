cbf7e68677955667b6308c99dda973050b0e1a41
Malhar Patel <malhar30000@gmail.com>
2025-10-01 09:16:18 +0000

    Call server on path `/run-sim-search`, parse `stdout` correctly and set default values for function arguments

--- LeanCodePrompts/SimilaritySearch.lean
+++ LeanCodePrompts/SimilaritySearch.lean
@@ -1,7 +1,18 @@
-def callSimilaritySearch (descField : String) (query : String) (numSim : Nat) : IO String := do
-  let exePath := System.mkFilePath [".", "SimilaritySearch", "SimilaritySearch.py"]
-  let inp ← IO.Process.output {cmd := "python3", args := #[exePath.toString, query, (toString numSim), descField]}
+import Lean
+
+def callSimilaritySearch (descField : String := "docString") (query : String := "mathematics") (numSim : Nat := 10) : IO String := do
+  let APIUrl := "http://localhost:7654/run-sim-search"
+  let js := Lean.Json.mkObj [("num", numSim), ("query", query), ("descField", descField)]
+  let inp ← IO.Process.output {cmd := "curl", args := #["-X", "POST", APIUrl, "-H", "Content-Type: application/json", "-d", js.compress]}
   let ⟨err_code,stdout,stderr⟩ := inp
   match err_code with
-  | 0 => return stdout
+  | 0 =>
+    match Lean.Json.parse stdout with
+    | Except.error e => return e
+    | Except.ok response =>
+      match response.getObjValAs? String "output" with
+      | Except.error e => return e
+      | Except.ok output => return output
   | _ => return stderr
+
+--#eval callSimilaritySearch "docString" "infinite odd primes" 5
