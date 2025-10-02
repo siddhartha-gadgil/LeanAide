bd2b6270cbca03609bb5c52f4517806b7a86eee0
ajay-k-nair <anir71@gmail.com>
2025-10-02 15:27:35 +0530

    Added readableJson function to Aides.lean

--- JSONtoYAML.lean
+++ JSONtoYAML.lean
@@ -10,7 +10,6 @@ partial def readableJson (js: Json) : Json :=
    match js with
   | Json.obj m =>
    match m.toArray with
-   | #[⟨k,v⟩] => Json.mkObj [(k, readableJson v)]
    | jsArr =>
      let keyVals := jsArr.map (fun ⟨k, v⟩ => (k, v))
      let purged := jsArr.filter (fun ⟨k, _⟩ => k != "type")
@@ -23,9 +22,11 @@ partial def readableJson (js: Json) : Json :=
        | some type =>
           Json.mkObj [(type, readableJson (Json.mkObj purged.toList))]
        | none => js
-     | none => js
+     | none =>
+       let keyValsModified := keyVals.map (fun (k,v) => (k, readableJson v))
+       Json.mkObj keyValsModified.toList
   | Json.arr m => (m.map (fun x => readableJson x)).toJson
-  | _ => Json.null
+  | _ => js
 
 #eval readableJson (json% {"type": "theorem-proof",
   "theorem": {"type": "val","win":"lose"},
@@ -97,61 +98,43 @@ partial def readableJson (js: Json) : Json :=
   }
 })
 
-#eval json% {
-  "document": {
-    "type": "document",
-    "body": [
-      {
-        "type": "Theorem",
-        "header": "Theorem",
-        "label": "thm:even_prime",
-        "claim": "The only even prime number is 2.",
-        "proof": [
-          {
-            "type": "assert_statement",
-            "claim": "An even number is any integer that is a multiple of 2."
-          },
-          {
-            "type": "assert_statement",
-            "claim": "A prime number is a natural number greater than 1 that has only two divisors: 1 and itself."
-          },
-          {
-            "type": "assert_statement",
-            "claim": "2 is even."
-          },
-          {
-            "type": "assert_statement",
-            "claim": "The only divisors of 2 are 1 and 2."
-          },
-          {
-            "type": "assert_statement",
-            "claim": "Therefore, 2 is prime."
-          },
-          {
-            "type": "let_statement",
-            "variable_name": "N",
-            "variable_type": "integer",
-            "properties": "even and greater than 2",
-            "statement": "Let N be an even number greater than 2."
-          },
-          {
-            "type": "assert_statement",
-            "claim": "N is divisible by 2."
-          },
-          {
-            "type": "assert_statement",
-            "claim": "The divisors of N include 1, 2, and N."
-          },
-          {
-            "type": "assert_statement",
-            "claim": "Since N has at least three distinct divisors, it is not prime."
-          },
-          {
-            "type": "conclude_statement",
-            "claim": "2 is the only even prime number."
-          }
-        ]
-      }
-    ]
-  }
-}
+/-YAML-like syntax for Lean-/
+namespace Lean.YAML
+
+/-YAML syntactic category-/
+declare_syntax_cat yaml
+
+/-YAML null syntax-/
+syntax "null" : yaml
+
+/-YAML true syntax-/
+syntax "true" : yaml
+
+/-YAML false syntax-/
+syntax "false" : yaml
+
+/-YAML string syntax-/
+syntax str : yaml
+
+/-YAML keys can be identifiers or strings-/
+syntax yamlIdent := ident <|> str
+
+/-YAML key-value pairs-/
+syntax manyIndent(yamlIdent":" yaml linebreak): yaml
+
+/-YAML syntax for lists-/
+syntax manyIndent("-" yaml ppLine linebreak): yaml
+
+/-Alternative YAML syntax for lists-/
+syntax "[" yaml,* "]": yaml
+
+
+def newstx : CoreM (Syntax) := `(yaml| -"key" : "val"
+                                       - "key2" : "val2"
+)
+
+def stxprint : CoreM Format := do
+  let stx ← newstx
+  PrettyPrinter.ppCategory `yaml stx
+
+#eval stxprint
--- LeanAideCore/LeanAideCore/Aides.lean
+++ LeanAideCore/LeanAideCore/Aides.lean
@@ -957,3 +957,184 @@ def proxyJson {α : Type} [inst: Proxy α] (a : α) : TermElabM Json := do
 def unproxyJson {β : Type} [FromJson β] [inst: InverseProxy β] (j: Json) : TermElabM inst.α := do
   let .ok (b : β) := fromJson? j | throwError s!"failed to parse {j}"
   unproxy b
+
+partial def readableJson (js: Json) : Json :=
+   match js with
+  | Json.obj m =>
+   match m.toArray with
+   | jsArr =>
+     let keyVals := jsArr.map (fun ⟨k, v⟩ => (k, v))
+     let purged := jsArr.filter (fun ⟨k, _⟩ => k != "type")
+     let purged := purged.map fun ⟨k, v⟩ => (k, v)
+     let typeVal? := keyVals.findSome? (fun (k, v) => if k == "type" then some v else none)
+     match typeVal? with
+     | some typeVal =>
+       let type? := typeVal.getStr?.toOption
+       match type? with
+       | some type =>
+          Json.mkObj [(type, readableJson (Json.mkObj purged.toList))]
+       | none => js
+     | none =>
+       let keyValsModified := keyVals.map (fun (k,v) => (k, readableJson v))
+       Json.mkObj keyValsModified.toList
+  | Json.arr m => (m.map (fun x => readableJson x)).toJson
+  | _ => js
+
+
+/--
+info: {"theorem-proof":
+ {"theorem": {"val": {"statement": "This is a Theorem"}}, "proof": "magic"}}
+-/
+#guard_msgs in
+#eval readableJson (json% {"type": "theorem-proof",
+  "theorem": {"type": "val","statement":"This is a Theorem"},
+  "proof" : "magic"
+})
+
+/--
+info: {"document":
+ [{"Theorem":
+   {"proof":
+    {"Proof":
+     {"proof_steps":
+      [[{"pattern_cases_statement":
+         {"proof_cases":
+          [{"pattern_case":
+            {"proof":
+             {"Proof":
+              {"proof_steps":
+               [[{"let_statement":
+                  {"variable_type": "integer",
+                   "variable_name": "k",
+                   "statement": "k is an integer such that n = 4k"}},
+                 {"calculation":
+                  {"calculation_sequence":
+                   ["n^2 = (4k)^2 = 16k^2", "16k^2 ≡ 0 (mod 4)"]}},
+                 {"conclude_statement": {"claim": "n^2 ≡ 0 (mod 4)"}}]],
+               "claim_label": "thm:mod4-square"}},
+             "pattern": "0"}},
+           {"pattern_case":
+            {"proof":
+             {"Proof":
+              {"proof_steps":
+               [[{"let_statement":
+                  {"variable_type": "integer",
+                   "variable_name": "k",
+                   "statement": "k is an integer such that n = 4k + 1"}},
+                 {"calculation":
+                  {"calculation_sequence":
+                   ["n^2 = (4k + 1)^2 = 16k^2 + 8k + 1",
+                    "16k^2 + 8k + 1 ≡ 1 (mod 4)"]}},
+                 {"conclude_statement": {"claim": "n^2 ≡ 1 (mod 4)"}}]],
+               "claim_label": "thm:mod4-square"}},
+             "pattern": "1"}},
+           {"pattern_case":
+            {"proof":
+             {"Proof":
+              {"proof_steps":
+               [[{"let_statement":
+                  {"variable_type": "integer",
+                   "variable_name": "k",
+                   "statement": "k is an integer such that n = 4k + 2"}},
+                 {"calculation":
+                  {"calculation_sequence":
+                   ["n^2 = (4k + 2)^2 = 16k^2 + 16k + 4",
+                    "16k^2 + 16k + 4 ≡ 0 (mod 4)"]}},
+                 {"conclude_statement": {"claim": "n^2 ≡ 0 (mod 4)"}}]],
+               "claim_label": "thm:mod4-square"}},
+             "pattern": "2"}},
+           {"pattern_case":
+            {"proof":
+             {"Proof":
+              {"proof_steps":
+               [[{"let_statement":
+                  {"variable_type": "integer",
+                   "variable_name": "k",
+                   "statement": "k is an integer such that n = 4k + 3"}},
+                 {"calculation":
+                  {"calculation_sequence":
+                   ["n^2 = (4k + 3)^2 = 16k^2 + 24k + 9",
+                    "16k^2 + 24k + 9 ≡ 1 (mod 4)"]}},
+                 {"conclude_statement": {"claim": "n^2 ≡ 1 (mod 4)"}}]],
+               "claim_label": "thm:mod4-square"}},
+             "pattern": "3"}}],
+          "on": "n mod 4"}},
+        {"conclude_statement": {"claim": "n^2 mod 4 ∈ {0, 1}"}}]],
+      "claim_label": "thm:mod4-square"}},
+    "label": "thm:mod4-square",
+    "header": "Theorem",
+    "claim": "For any integer n, n^2 mod 4 ∈ {0, 1}"}}]}
+-/
+#guard_msgs in
+#eval readableJson (json% {"document":
+ [{"type": "Theorem",
+   "proof":
+   {"type": "Proof",
+    "proof_steps":
+    [[{"type": "pattern_cases_statement",
+       "proof_cases":
+       [{"type": "pattern_case",
+         "proof":
+         {"type": "Proof",
+          "proof_steps":
+          [[{"variable_type": "integer",
+             "variable_name": "k",
+             "type": "let_statement",
+             "statement": "k is an integer such that n = 4k"},
+            {"type": "calculation",
+             "calculation_sequence":
+             ["n^2 = (4k)^2 = 16k^2", "16k^2 ≡ 0 (mod 4)"]},
+            {"type": "conclude_statement", "claim": "n^2 ≡ 0 (mod 4)"}]],
+          "claim_label": "thm:mod4-square"},
+         "pattern": "0"},
+        {"type": "pattern_case",
+         "proof":
+         {"type": "Proof",
+          "proof_steps":
+          [[{"variable_type": "integer",
+             "variable_name": "k",
+             "type": "let_statement",
+             "statement": "k is an integer such that n = 4k + 1"},
+            {"type": "calculation",
+             "calculation_sequence":
+             ["n^2 = (4k + 1)^2 = 16k^2 + 8k + 1",
+              "16k^2 + 8k + 1 ≡ 1 (mod 4)"]},
+            {"type": "conclude_statement", "claim": "n^2 ≡ 1 (mod 4)"}]],
+          "claim_label": "thm:mod4-square"},
+         "pattern": "1"},
+        {"type": "pattern_case",
+         "proof":
+         {"type": "Proof",
+          "proof_steps":
+          [[{"variable_type": "integer",
+             "variable_name": "k",
+             "type": "let_statement",
+             "statement": "k is an integer such that n = 4k + 2"},
+            {"type": "calculation",
+             "calculation_sequence":
+             ["n^2 = (4k + 2)^2 = 16k^2 + 16k + 4",
+              "16k^2 + 16k + 4 ≡ 0 (mod 4)"]},
+            {"type": "conclude_statement", "claim": "n^2 ≡ 0 (mod 4)"}]],
+          "claim_label": "thm:mod4-square"},
+         "pattern": "2"},
+        {"type": "pattern_case",
+         "proof":
+         {"type": "Proof",
+          "proof_steps":
+          [[{"variable_type": "integer",
+             "variable_name": "k",
+             "type": "let_statement",
+             "statement": "k is an integer such that n = 4k + 3"},
+            {"type": "calculation",
+             "calculation_sequence":
+             ["n^2 = (4k + 3)^2 = 16k^2 + 24k + 9",
+              "16k^2 + 24k + 9 ≡ 1 (mod 4)"]},
+            {"type": "conclude_statement", "claim": "n^2 ≡ 1 (mod 4)"}]],
+          "claim_label": "thm:mod4-square"},
+         "pattern": "3"}],
+       "on": "n mod 4"},
+      {"type": "conclude_statement", "claim": "n^2 mod 4 ∈ {0, 1}"}]],
+    "claim_label": "thm:mod4-square"},
+   "label": "thm:mod4-square",
+   "header": "Theorem",
+   "claim": "For any integer n, n^2 mod 4 ∈ {0, 1}"}]})
