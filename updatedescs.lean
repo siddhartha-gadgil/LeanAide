4fd3937b117d9b0a4d6a6b37e65107146de93795
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-10-03 02:15:13 +0000

    updated description data

--- updatedescs.lean
+++ updatedescs.lean
@@ -23,13 +23,18 @@ unsafe def main : IO Unit := do
   let jsLines := lines.filterMap fun line =>
     (Json.parse line).toOption
   IO.eprintln s!"Read {jsLines.size} lines (after filtering) from {(lines.size)}"
-  let h ← IO.FS.Handle.mk ((← resourcesDir) / "mathlib4-prompts.jsonl") IO.FS.Mode.write
+  let h ← IO.FS.Handle.mk ((← resourcesDir) / "mathlib4-descs.jsonl") IO.FS.Mode.write
   let mut count := 0
   for js in jsLines do
     count := count + 1
     if count % 1000 == 0 then IO.eprintln s!"Processing {count} lines"
+    let .ok name := js.getObjValAs? Name "name" | throw <| IO.userError s!"Failed to parse JSON: {js}"
     let core := updateDescCore? js
     let js? ← core.run' coreContext {env := env} |>.runToIO'
     match js? with
-    | none => IO.eprintln s!"No update for {js}"
-    | some js => h.putStrLn js.compress
+    | none =>
+      -- IO.eprintln s!"No update for {name}"
+      continue
+    | some js =>
+      -- IO.eprintln s!"Updated {name}"
+      h.putStrLn js.compress
