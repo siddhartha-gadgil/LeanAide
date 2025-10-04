930ec84083c6ea0fb91c77321fd37b6c753abc32
Malhar Patel <malhar30000@gmail.com>
2025-10-04 15:38:16 +0000

    Create separate function `modify_js` to add distance field and rename key field name to `docString`

--- SimilaritySearch/similarity_search.py
+++ SimilaritySearch/similarity_search.py
@@ -6,8 +6,7 @@ import json
 import faiss
 from sentence_transformers import SentenceTransformer
 
-# Config the model and file paths
-DIM = 768
+# Config the file paths
 DESCFIELD_PATHS = {
     "docString" : "resources/mathlib4-prompts_new.jsonl",
     "concise-description" : "resources/mathlib4-descs_new.jsonl",
@@ -24,21 +23,36 @@ FIELD_NAME = {
     "description" : "description"
 }
 
+DIM = 768 # If changed, add `truncate_dim = DIM` to model.encode function
+
 def check_GPU():
   try:
       res = faiss.StandardGpuResources()
       index = faiss.GpuIndexFlatL2(res, 10)
-      print("FAISS GPU index created successfully.")
+      print("FAISS GPU index created!")
   except Exception as e:
       print(f"Failed to create FAISS GPU index: {e}")
 
 def get_data(descField, idx):
     with open(DESCFIELD_PATHS[descField], "r", encoding="utf-8") as file:
         for line_num, line in enumerate(file):
+            # index idx corresponds to line_num
             if line_num == idx:
                 data = json.loads(line)
                 return data
     return None
+
+def modify_js(js, descField, distance):
+    # Add distance field
+    js["distance"] = float(distance)
+    # Get FIELD_NAME[descField] value
+    temp = js[FIELD_NAME[descField]]
+    # Delete "docString" (in case of description and concise-description files) and FIELD_NAME[descField]
+    if "docString" in js: del js["docString"]
+    if FIELD_NAME[descField] in js: del js[FIELD_NAME[descField]]
+    # Essentially, rename FIELD_NAME[descField] to "docString"
+    js["docString"] = temp
+    return js
 
 def similarity_search(query, model, index, descField, num):
     # Encode the query theorem into a vector
@@ -49,13 +63,8 @@ def similarity_search(query, model, index, descField, num):
     for i, idx in enumerate(indices[0]):
         # Get the data on line (idx + 1)
         js = get_data(descField, idx)
-        # Add distance field
-        js["distance"] = float(distances[0][i])
-        # Rename FIELD_NAME[descField] to "docString"
-        temp = js[FIELD_NAME[descField]]
-        if "docString" in js: del js["docString"]
-        if FIELD_NAME[descField] in js: del js[FIELD_NAME[descField]]
-        js["docString"] = temp
+        # Modify js
+        js = modify_js(js, descField, distances[0][i])
         output.append(js)
     js_string = json.dumps(output)
     return js_string
