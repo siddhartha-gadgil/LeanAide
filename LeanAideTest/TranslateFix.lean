c9620ca3b84b0836e6a8cdf6bb38e9d65a8538e4
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-10-13 15:39:52 +0000

    reduce translate dependencies

--- LeanAideTest/TranslateFix.lean
+++ LeanAideTest/TranslateFix.lean
@@ -2,6 +2,7 @@ import Mathlib
 import LeanAide.Actor
 import LeanAide.ReTranslate
 import LeanAide.ReTranslators
+import LeanCodePrompts.Translate
 
 open LeanAide Lean Meta Translate
 
@@ -11,6 +12,6 @@ def server := ({} : Translator).server
 
 -- #eval server.mathCompletions <| (existsFixPrompt? exCode).get!
 
-#eval retranslateFromErrors #[.unparsed exCode "<dummy>" none]
+#eval retranslateFromErrors (greedyBestExprWithErr?) #[.unparsed exCode "<dummy>" none]
 
 #eval allRetranslatePrompts "∃ (G : Type) [inst : Group G], ∀ g : G, g * g = 1"
