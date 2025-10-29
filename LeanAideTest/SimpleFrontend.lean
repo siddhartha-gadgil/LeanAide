93c7cb90432128ce5f2ea9a6bdc1bd596e6bb2ca
ajay-k-nair <anir71@gmail.com>
2025-10-29 16:39:28 +0530

    added more tests

--- LeanAideTest/SimpleFrontend.lean
+++ LeanAideTest/SimpleFrontend.lean
@@ -13,6 +13,17 @@ namespace LeanAideTest
 #guard_msgs in
 #eval newDeclarations "def x : Nat := 0"
 
+/-- info: #[`newTheorem] -/
+#guard_msgs in
+#eval newDeclarations "def newTheorem (G : Type u) [Group G] : Type u := G ⧸ commutator G"
+
+/-- info: #[`M, `x, `newTheorem] -/
+#guard_msgs in
+#eval newDeclarations "def M {R₁ : Type u_1} [CommSemiring R₁] {n : Type u_5} [Fintype n] (M : Matrix n n R₁) : LinearMap.BilinForm R₁ (n → R₁) :=
+  Matrix.toLinearMap₂'Aux _ _ M
+  def newTheorem (G : Type u) [Group G] : Type u := G ⧸ commutator G
+  def x : Nat := 0"
+
 elab "#new_defs" s:str : command => Command.liftTermElabM do
   let s := s.getString
   let (nameDefs, msgs) ← elabFrontDefsNewExprM s
@@ -21,9 +32,16 @@ elab "#new_defs" s:str : command => Command.liftTermElabM do
   for msg in msgs.toList do
     logInfo msg.data
 
+/--
+info: New definition: IsContinuous with value fun {X} {Y} [TopologicalSpace X] [TopologicalSpace Y] Φ => ∀ (s : Set Y), IsOpen s → IsOpen (Φ ⁻¹' s)
+-/
+#guard_msgs in
+#new_defs "open Nat def IsContinuous {X Y : Type*} [TopologicalSpace X] [TopologicalSpace Y] (Φ : X → Y) : Prop := ∀ s : Set Y, IsOpen s → IsOpen (Φ⁻¹' s)"
+
+
 /-- info: New definition: y with value 1 -/
 #guard_msgs in
-#new_defs "def y : Nat := 1"
+#new_defs "open Nat def y : Nat := 1"
 
 /--
 info: New definition: M with value fun {R₁} [CommSemiring R₁] {n} [Fintype n] M => Matrix.toLinearMap₂'Aux (RingHom.id R₁) (RingHom.id R₁) M
@@ -31,6 +49,70 @@ info: New definition: M with value fun {R₁} [CommSemiring R₁] {n} [Fintype n
 #guard_msgs in
 #new_defs "def M {R₁ : Type u_1} [CommSemiring R₁] {n : Type u_5} [Fintype n] (M : Matrix n n R₁) : LinearMap.BilinForm R₁ (n → R₁) :=
   Matrix.toLinearMap₂'Aux _ _ M"
+
+
+/--
+info: New definition: mWCs with value fun 𝕜 [NontriviallyNormedField 𝕜] E [inst_1 : NormedAddCommGroup E] [NormedSpace 𝕜 E] =>
+  ModelWithCorners.of_target_univ 𝕜 (PartialEquiv.refl E) (@mWCs._proof_1 E) (@mWCs._proof_2 E)
+    (@mWCs._proof_3 E inst_1) (@mWCs._proof_3 E inst_1)
+---
+info: New definition: MWc with value fun 𝕜 [inst : NontriviallyNormedField 𝕜] {E} [inst_1 : NormedAddCommGroup E] [inst_2 : NormedSpace 𝕜 E] {H}
+    [TopologicalSpace H] f hsource htarget hcont hcont_inv =>
+  { toPartialEquiv := f, source_eq := hsource,
+    convex_range' := @MWc._proof_1 𝕜 inst E inst_1 inst_2 H f hsource htarget,
+    nonempty_interior' := @MWc._proof_2 E inst_1 H f hsource htarget, continuous_toFun := hcont,
+    continuous_invFun := hcont_inv }
+-/
+#guard_msgs in
+#new_defs "def MWc (𝕜 : Type u_1) [NontriviallyNormedField 𝕜] {E : Type u_2} [NormedAddCommGroup E] [NormedSpace 𝕜 E] {H : Type u_3} [TopologicalSpace H] (f : PartialEquiv H E) (hsource : f.source = Set.univ) (htarget : f.target = Set.univ) (hcont : Continuous ↑f) (hcont_inv : Continuous ↑f.symm) :
+ModelWithCorners 𝕜 E H where
+  toPartialEquiv := f
+  source_eq := hsource
+  convex_range' := by
+    have : Set.range f = f.target := by rw [← f.image_source_eq_target, hsource, Set.image_univ.symm]
+    simp only [this, htarget, dite_else_true]
+    intro h
+    letI := h.rclike 𝕜
+    letI := NormedSpace.restrictScalars ℝ 𝕜 E
+    exact convex_univ
+  nonempty_interior' := by
+    have : Set.range f = f.target := by rw [← f.image_source_eq_target, hsource, Set.image_univ.symm]
+    simp [this, htarget]
+
+    def mWCs (𝕜 : Type*) [NontriviallyNormedField 𝕜] (E : Type*)
+    [NormedAddCommGroup E] [NormedSpace 𝕜 E] : ModelWithCorners 𝕜 E E :=
+  ModelWithCorners.of_target_univ 𝕜 (PartialEquiv.refl E) rfl rfl continuous_id continuous_id
+    "
+/--
+info: New definition: MWCReqT with value fun {𝕜} {E} {H} [NontriviallyNormedField 𝕜] [NormedAddCommGroup E] [NormedSpace 𝕜 E] [TopologicalSpace H] I =>
+  Eq.mpr
+    (id
+      (congrArg (fun _a => Set.range ↑I.toPartialEquiv = _a)
+        (Eq.symm (PartialEquiv.image_source_eq_target I.toPartialEquiv))))
+    (Eq.mpr (id (congrArg (fun _a => Set.range ↑I.toPartialEquiv = ↑I.toPartialEquiv '' _a) I.source_eq))
+      (Eq.mpr (id (congrArg (fun _a => _a = ↑I.toPartialEquiv '' Set.univ) (Eq.symm Set.image_univ)))
+        (Eq.refl (↑I.toPartialEquiv '' Set.univ))))
+-/
+#guard_msgs in
+#new_defs "theorem MWCReqT{𝕜 : Type u_1} {E : Type u_2} {H : Type u_3} [NontriviallyNormedField 𝕜] [NormedAddCommGroup E] [NormedSpace 𝕜 E] [TopologicalSpace H] (I : ModelWithCorners 𝕜 E H) :
+Set.range ↑I.toPartialEquiv = I.target := by
+  rw [← I.image_source_eq_target, I.source_eq, Set.image_univ.symm]"
+
+/--
+info: New definition: CMap with value fun {C} [CategoryTheory.Category.{v₁, u₁} C] {D} [CategoryTheory.Category.{v₂, u₂} D] {E}
+    [CategoryTheory.Category.{v₃, u₃} E] F G {X Y} f =>
+  rfl
+---
+info: New definition: CTFC with value fun {C} [CategoryTheory.Category.{v₁, u₁} C] {D} [CategoryTheory.Category.{v₂, u₂} D] F {X Y} {f g} h =>
+  Eq.mpr (id (congrArg (fun _a => F.map _a = F.map g) h)) (Eq.refl (F.map g))
+-/
+#guard_msgs in
+#new_defs "open CategoryTheory theorem CTFC {C : Type u₁} [CategoryTheory.Category.{v₁ ,u₁} C] {D : Type u₂} [Category.{v₂, u₂} D] (F : C ⥤ D) {X Y : C} {f g : X ⟶ Y} (h : f = g) :
+F.map f = F.map g := by
+ rw[h]
+theorem CMap {C : Type u₁} [Category.{v₁, u₁} C] {D : Type u₂} [Category.{v₂, u₂} D] {E : Type u₃} [Category.{v₃, u₃} E](F : C ⥤ D) (G : D ⥤ E) {X Y : C} (f : X ⟶ Y) :
+    (F ⋙ G).map f = G.map (F.map f) := rfl
+ "
 
 /--
 info: Except.ok (Lean.Expr.forallE
@@ -330,6 +412,13 @@ cases h with
   · exact hp
 "  `LinearMap.BilinForm
 
+
+#new_defs "open CategoryTheory theorem CTFC {C : Type u₁} [CategoryTheory.Category.{v₁ ,u₁} C] {D : Type u₂} [Category.{v₂, u₂} D] (F : C ⥤ D) {X Y : C} {f g : X ⟶ Y} (h : f = g) :
+F.map f = F.map g := by
+ rw[h]
+theorem CMap {C : Type u₁} [Category.{v₁, u₁} C] {D : Type u₂} [Category.{v₂, u₂} D] {E : Type u₃} [Category.{v₃, u₃} E](F : C ⥤ D) (G : D ⥤ E) {X Y : C} (f : X ⟶ Y) :
+    (F ⋙ G).map f = G.map (F.map f) := rfl
+ "
 
 
 end LeanAideTest
