1819352d18d14abc0c220fae7672f5df1b663ebd
shubhamkumar13 <kumar.shubham.off@gmail.com>
2025-11-06 06:08:47 +0530

    * Use polyTrace classes in PaperCodes.lean
    * [DONE] : create polyTrace classes for FILE and IO logging, added test to check FILE logging
    
    * [TODO] : add tests for polyTraceIO

--- LeanAideCore/LeanAideCore/Config.lean
+++ LeanAideCore/LeanAideCore/Config.lean
@@ -7,6 +7,8 @@ initialize
   registerTraceClass `Translate.warning
   registerTraceClass `leanaide.proof.info
   registerTraceClass `leanaide.codegen.info
+  registerTraceClass `PaperCodes.info
+  registerTraceClass `PaperCodes.error
 
 register_option leanaide.logging : Bool :=
   { defValue := false
@@ -54,3 +56,60 @@ def resourcesDir : IO System.FilePath := do
   return base / "resources"
 
 -- #eval resourcesDir
+
+initialize polyTraceIO : IO.Ref Bool ← IO.mkRef false
+initialize polyTraceFile : IO.Ref Bool ← IO.mkRef false
+
+namespace polyTraceIO
+  def on (_ : Unit) : IO Unit := do
+    let current ← polyTraceIO.get
+    if current then pure () else polyTraceIO.set true
+
+  def off (_ : Unit) : IO Unit := do
+    let current ← polyTraceIO.get
+    if current then polyTraceIO.set false else pure ()
+
+  def status (_ : Unit) : IO Bool := do
+    (← polyTraceIO.get)
+    |> pure
+
+end polyTraceIO
+
+namespace polyTraceFile
+  def on (_ : Unit) : IO Unit := do
+    let current ← polyTraceFile.get
+    if current then pure () else polyTraceFile.set true
+
+  def off (_ : Unit) : IO Unit := do
+    let current ← polyTraceFile.get
+    if current then polyTraceFile.set false
+
+  def status (_ : Unit) : IO Bool := do
+    (← polyTraceFile.get)
+    |> pure
+
+end polyTraceFile
+
+def polyTrace (tag : Name) (msg : String) : CoreM Unit := do
+-- always print trace
+  Lean.trace tag (fun _ => msg)
+-- use mkRef to globally update this
+  let isIO ← liftM <| polyTraceIO.status ()
+  let isFile ← liftM <| polyTraceFile.status ()
+
+  match isIO, isFile with
+  | false, false =>
+      throwError "Invalid State : Both IO and File switches are turned on or both are turned off"
+  | true, false =>
+      IO.eprintln s!"[{tag.toString}] [IO] {msg}"
+  | false, true =>
+      let currentDir ← IO.currentDir
+      let logFilePath := System.mkFilePath [currentDir.toString, "output.log"]
+      IO.eprintln s!"The output is logged to {logFilePath}"
+      IO.FS.writeFile logFilePath s!"[File] {msg}"
+  | true, true =>
+      IO.eprintln s!"[{tag.toString}] [IO] {msg}"
+      let currentDir ← IO.currentDir
+      let logFilePath := System.mkFilePath [currentDir.toString, "output.log"]
+      IO.eprintln s!"The output is logged to {logFilePath}"
+      IO.FS.writeFile logFilePath s!"[File] {msg}"
