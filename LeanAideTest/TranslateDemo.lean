13c19e8c9eaddfdbef8f3ab0625bc28ef5095bea
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-12-22 07:14:58 +0530

    demo state

--- LeanAideTest/TranslateDemo.lean
+++ LeanAideTest/TranslateDemo.lean
@@ -6,7 +6,7 @@ set_option lean_aide.translate.greedy true
 
 #theorem silly "If a vector space has dimension `2` then it is finite dimensional."
 
-#theorem : "There are infinitely many odd numbers." >> translate_theorem
+#theorem : "There are infinitely many odd numbers."
 
 #llm_query "Prove that there are infinitely many even numbers."
 
